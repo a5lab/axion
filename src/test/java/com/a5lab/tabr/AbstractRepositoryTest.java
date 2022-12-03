714b1197f860bcba38cd105f75c876ea09ca35a6
Yuriy Chervonyi <zolotoych@yandex.ru>
2022-12-04 02:29:53 +0300

    Added support for Ring and Segment

---
+++ src/test/java/com/a5lab/tabr/AbstractRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.a5lab.tabr;
+
+import com.a5lab.tabr.config.JpaAuditingConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import({
+    JpaAuditingConfiguration.class
+})
+@TabrTestBaseConfig
+public abstract class AbstractRepositoryTest {
+}
---
+++ src/test/java/com/a5lab/tabr/domain/segments/SegmentRepositoryTest.java
@@ -0,0 +1,95 @@
+package com.a5lab.tabr.domain.segments;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.a5lab.tabr.AbstractRepositoryTest;
+import jakarta.validation.ValidationException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class SegmentRepositoryTest extends AbstractRepositoryTest {
+
+  @Autowired
+  private SegmentRepository segmentRepository;
+
+  @Test
+  void shouldSaveSegmentWithTitleAndDescription() {
+    final Segment s = new Segment();
+    s.setTitle("My new test Segment");
+    s.setDescription("My awesome description");
+
+    Assertions.assertNull(s.getId());
+    segmentRepository.saveAndFlush(s);
+    Assertions.assertNotNull(s.getId());
+    Assertions.assertNotNull(s.getCreatedBy());
+    Assertions.assertNotNull(s.getCreatedDate());
+    Assertions.assertNotNull(s.getLastModifiedBy());
+    Assertions.assertNotNull(s.getLastModifiedDate());
+  }
+
+  @Test
+  void shouldFailOnNullTitle() {
+    final Segment s = new Segment();
+    s.setDescription("My awesome description");
+
+    Assertions.assertNull(s.getId());
+    assertThatThrownBy(() -> segmentRepository.saveAndFlush(s))
+        .isInstanceOf(ValidationException.class);
+  }
+
+  @Test
+  void shouldFailOnNullDescription() {
+    final Segment s = new Segment();
+    s.setTitle("My new test Segment");
+
+    Assertions.assertNull(s.getId());
+    assertThatThrownBy(() -> segmentRepository.saveAndFlush(s))
+        .isInstanceOf(ValidationException.class);
+  }
+
+  @Test
+  void shouldFailOnEmptyTitle() {
+    final Segment s = new Segment();
+    s.setTitle("");
+    s.setDescription("My awesome description");
+
+    Assertions.assertNull(s.getId());
+    assertThatThrownBy(() -> segmentRepository.saveAndFlush(s))
+        .isInstanceOf(ValidationException.class);
+  }
+
+  @Test
+  void shouldFailOnWhiteSpaceTitle() {
+    final Segment s = new Segment();
+    s.setTitle(" ");
+    s.setDescription("My awesome description");
+
+    Assertions.assertNull(s.getId());
+    assertThatThrownBy(() -> segmentRepository.saveAndFlush(s))
+        .isInstanceOf(ValidationException.class);
+  }
+
+  @Test
+  void shouldFailOnEmptyDescription() {
+    final Segment s = new Segment();
+    s.setTitle("Hello");
+    s.setDescription("");
+
+    Assertions.assertNull(s.getId());
+    assertThatThrownBy(() -> segmentRepository.saveAndFlush(s))
+        .isInstanceOf(ValidationException.class);
+  }
+
+  @Test
+  void shouldFailOnWhiteSpaceDescription() {
+    final Segment s = new Segment();
+    s.setTitle("Hello");
+    s.setDescription(" ");
+
+    Assertions.assertNull(s.getId());
+    assertThatThrownBy(() -> segmentRepository.saveAndFlush(s))
+        .isInstanceOf(ValidationException.class);
+  }
+
+}
