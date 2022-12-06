f2181ef86779be7fbc139f4edb1300544ef6089b
Yuriy Chervonyi <zolotoych@yandex.ru>
2022-12-06 22:58:00 +0300

    servletPath into single place

--- src/main/java/com/a5lab/tabr/controllers/HomeController.java
+++ src/main/java/com/a5lab/tabr/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
   @GetMapping({"/", "index.html"})
-  public String main() {
+  public String index() {
     return "index.html";
   }
 
