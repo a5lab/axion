752fbff004e6c32840dfb11afb747d85774c3856
arufanov <arufanov@gmail.com>
2022-12-07 01:15:49 +0300

    intermediate commit

--- src/main/java/com/a5lab/tabr/controllers/WelcomeController.java
+++ src/main/java/com/a5lab/tabr/controllers/WelcomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController {
+public class WelcomeController {
 
   @GetMapping({"/", "welcome.html"})
   public ModelAndView main() {
