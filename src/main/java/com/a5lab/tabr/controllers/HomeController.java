ed8663c911d54bdad332ce7fe4b63b50cab54e23
arufanov <arufanov@gmail.com>
2022-12-11 00:19:39 +0300

    add radar

--- src/main/java/com/a5lab/tabr/controllers/HomeController.java
+++ src/main/java/com/a5lab/tabr/controllers/HomeController.java
@@ -1,14 +1,23 @@
 package com.a5lab.tabr.controllers;
 
+import com.a5lab.tabr.domain.rings.Radar;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController {
 
   @GetMapping({"/", "home.html"})
-  public String index() {
-    return "home.html";
+  public ModelAndView index() {
+    Radar radar = new Radar();
+    radar.setTitle("myTitle");
+    radar.setDescription("myDescriptions");
+
+    ModelAndView modelAndView = new ModelAndView("home");
+    modelAndView.addObject(radar);
+    return modelAndView;
   }
 
 }
