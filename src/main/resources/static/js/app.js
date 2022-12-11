a5fa34ff33b898404ece43f22a3f1fe6fca733ff
arufanov <arufanov@gmail.com>
2022-12-11 22:35:15 +0300

    87 fix java script warning (#92)
    Warning has been removed

--- src/main/resources/static/js/app.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.js-submit-confirm').forEach(($item) => {
-    $item.addEventListener('submit', (event) => {
-        if (!confirm(event.currentTarget.getAttribute('data-confirm-message'))) {
-            event.preventDefault();
-            return false;
-        }
-        return true;
-    });
-});
