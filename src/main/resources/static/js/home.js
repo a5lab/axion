8e37b588e78de63f96ef1f3640e497f8028e41be
arufanov <arufanov@gmail.com>
2022-12-14 01:12:20 +0300

    update index and java

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -2,49 +2,20 @@
 // Handler for home page
 
 document.addEventListener('DOMContentLoaded', () => {
-  // alert('Hello from home handler');
-  console.log('Hello from home handler.');
+  // Extract radar properties
+  // console.log(  d3.select("h1").text());
+  // console.log(  d3.select("p#name").text());
+  // console.log(  d3.select("p#description").text());
 
+  // Extract all segments
+  let segments = d3.selectAll("table#segments tr.data td.title");
+  console.log(  d3.selectAll("table#segments tr.data td.title"));
 
-  var data = [1, 1, 2, 3, 5, 8, 13];
-
-  var canvas = document.querySelector("canvas"),
-      context = canvas.getContext("2d");
-
-  var width = canvas.width,
-      height = canvas.height,
-      radius = Math.min(width, height) / 2;
-
-  var colors = [
-    "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-    "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"
-  ];
-
-  var arc = d3.arc()
-      .outerRadius(radius - 10)
-      .innerRadius(radius - 70)
-      .padAngle(0.03)
-      .context(context);
-
-  var pie = d3.pie();
-
-  var arcs = pie(data);
-
-  context.translate(width / 2, height / 2);
-
-  context.globalAlpha = 0.5;
-  arcs.forEach(function(d, i) {
-    context.beginPath();
-    arc(d);
-    context.fillStyle = colors[i];
-    context.fill();
+  segments.each(function (segments, j) {
+    // console.log("segment:" + segments[j]);
+    // console.log("segment:" + d3.select(this).select("td.title").text());
+    // console.log("j" + j);
   });
-
-  context.globalAlpha = 1;
-  context.beginPath();
-  arcs.forEach(arc);
-  context.lineWidth = 1.5;
-  context.stroke();
 
 });
 
