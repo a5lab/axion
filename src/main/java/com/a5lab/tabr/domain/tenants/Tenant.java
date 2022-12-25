75d9151d88b742ce4f3533f4a8f6098611dc6340
arufanov <arufanov@gmail.com>
2022-12-25 17:31:01 +0300

    add tenants

---
+++ src/main/java/com/a5lab/tabr/domain/tenants/Tenant.java
@@ -0,0 +1,51 @@
+package com.a5lab.tabr.domain.rings;
+
+import com.a5lab.tabr.domain.AbstractAuditable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Table(name = "rings")
+@DynamicUpdate
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Ring extends AbstractAuditable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, updatable = false, unique = true)
+  @Setter(AccessLevel.NONE)
+  private Integer id;
+
+  @NotBlank
+  @RingTitleConstraint
+  @Column(name = "title", unique = true, nullable = false)
+  private String title;
+
+  @NotBlank
+  @Column(name = "description", nullable = false)
+  private String description;
+
+  @Column(name = "info")
+  private String info;
+
+  @Column(name = "priority", nullable = false)
+  private int priority;
+
+  @Column(name = "is_active", nullable = false)
+  private boolean active = true;
+
+}
