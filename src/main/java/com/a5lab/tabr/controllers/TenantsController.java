55fdb138a84a14c2328313371ecdd0d3ea458d95
arufanov <arufanov@gmail.com>
2022-12-25 18:08:02 +0300

    update controller

--- src/main/java/com/a5lab/tabr/controllers/TenantsController.java
+++ src/main/java/com/a5lab/tabr/controllers/TenantsController.java
@@ -2,13 +2,21 @@ package com.a5lab.tabr.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import com.a5lab.tabr.domain.tenants.TenantRepository;
 
 @Controller
 public class TenantsController {
 
+  private final TenantRepository tenantRepository;
+
   @GetMapping("/settings/tenants")
-  public String index() {
-    return "/settings/tenants";
+  public ModelAndView index() {
+
+    ModelAndView modelAndView = new ModelAndView("/settings/tenants");
+    modelAndView.addObject(tenantRepository.findAll());
+    return modelAndView;
   }
+
 
 }
--- src/main/java/com/a5lab/tabr/domain/tenants/TenantRepository.java
+++ src/main/java/com/a5lab/tabr/domain/tenants/TenantRepository.java
@@ -1,11 +1,9 @@
 package com.a5lab.tabr.domain.tenants;
 
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TenantRepository extends JpaRepository<Tenant, Integer> {
-  Optional<Tenant> findTenantByTitle(String title);
+public interface TenantRepository extends CrudRepository<Tenant, Integer> {
 
 }
