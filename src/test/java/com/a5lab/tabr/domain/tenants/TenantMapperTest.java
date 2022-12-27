c8cd533d9aea9a4e468c701829c30159f1f31521
arufanov <arufanov@gmail.com>
2022-12-27 17:58:38 +0300

    add a new testts

--- src/test/java/com/a5lab/tabr/domain/tenants/TenantMapperTest.java
+++ src/test/java/com/a5lab/tabr/domain/tenants/TenantMapperTest.java
@@ -10,14 +10,27 @@ class TenantMapperTest {
   private final TenantMapper mapper = Mappers.getMapper(TenantMapper.class);
 
   @Test
-  void testToDtoWithNull() {
+  void testToRecordWithNull() {
     final var result = mapper.toRecord(null);
 
     Assertions.assertNull(result);
   }
 
   @Test
-  void testAllFields() throws NoSuchFieldException {
+  void testToRecordIdIsNull() throws NoSuchFieldException {
+    final Tenant t = new Tenant();
+    t.setTitle("my title1");
+    t.setDescription("my description1");
+
+    final var result = mapper.toRecord(t);
+
+    Assertions.assertNull(result.id());
+    Assertions.assertEquals(result.title(), t.getTitle());
+    Assertions.assertEquals(result.description(), t.getDescription());
+  }
+
+  @Test
+  void testToRecordAllFields() throws NoSuchFieldException {
     final Tenant t = new Tenant();
     var field = Tenant.class.getDeclaredField("id");
     ReflectionUtils.makeAccessible(field);
@@ -33,7 +46,14 @@ class TenantMapperTest {
   }
 
   @Test
-  void testIdIsNull() throws NoSuchFieldException {
+  void testToEntityWithNull() {
+    final var result = mapper.toRecord(null);
+
+    Assertions.assertNull(result);
+  }
+
+  @Test
+  void testToEntityIdIsNull() throws NoSuchFieldException {
     final Tenant t = new Tenant();
     t.setTitle("my title1");
     t.setDescription("my description1");
@@ -45,4 +65,19 @@ class TenantMapperTest {
     Assertions.assertEquals(result.description(), t.getDescription());
   }
 
+  @Test
+  void testToEntityAllFields() throws NoSuchFieldException {
+    final Tenant t = new Tenant();
+    var field = Tenant.class.getDeclaredField("id");
+    ReflectionUtils.makeAccessible(field);
+    ReflectionUtils.setField(field, t, 123L);
+    t.setTitle("my title");
+    t.setDescription("my description");
+
+    final var result = mapper.toRecord(t);
+
+    Assertions.assertEquals(result.id(), t.getId());
+    Assertions.assertEquals(result.title(), t.getTitle());
+    Assertions.assertEquals(result.description(), t.getDescription());
+  }
 }
