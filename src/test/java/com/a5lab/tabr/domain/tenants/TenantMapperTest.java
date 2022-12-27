50b947a0f260426256c429c3ce7f0afbe59082eb
arufanov <arufanov@gmail.com>
2022-12-27 18:05:54 +0300

    Rename chekcstyle to google_checks.xml

--- src/test/java/com/a5lab/tabr/domain/tenants/TenantMapperTest.java
+++ src/test/java/com/a5lab/tabr/domain/tenants/TenantMapperTest.java
@@ -17,7 +17,7 @@ class TenantMapperTest {
   }
 
   @Test
-  void testToRecordIdIsNull() throws NoSuchFieldException {
+  void testToRecordIdIsNull() {
     final Tenant t = new Tenant();
     t.setTitle("my title1");
     t.setDescription("my description1");
@@ -47,37 +47,30 @@ class TenantMapperTest {
 
   @Test
   void testToEntityWithNull() {
-    final var result = mapper.toRecord(null);
+    final var result = mapper.toEntity(null);
 
     Assertions.assertNull(result);
   }
 
   @Test
-  void testToEntityIdIsNull() throws NoSuchFieldException {
-    final Tenant t = new Tenant();
-    t.setTitle("my title1");
-    t.setDescription("my description1");
+  void testToEntityIdIsNull() {
+    final TenantRecord record = new TenantRecord(null, "my title1", "my description1");
 
-    final var result = mapper.toRecord(t);
+    final var result = mapper.toEntity(record);
 
-    Assertions.assertNull(result.id());
-    Assertions.assertEquals(result.title(), t.getTitle());
-    Assertions.assertEquals(result.description(), t.getDescription());
+    Assertions.assertNull(result.getId());
+    Assertions.assertEquals(result.getTitle(), record.title());
+    Assertions.assertEquals(result.getDescription(), record.description());
   }
 
   @Test
-  void testToEntityAllFields() throws NoSuchFieldException {
-    final Tenant t = new Tenant();
-    var field = Tenant.class.getDeclaredField("id");
-    ReflectionUtils.makeAccessible(field);
-    ReflectionUtils.setField(field, t, 123L);
-    t.setTitle("my title");
-    t.setDescription("my description");
+  void testToEntityAllFields() {
+    final TenantRecord record = new TenantRecord(1L, "my title1", "my description1");
 
-    final var result = mapper.toRecord(t);
+    final var result = mapper.toEntity(record);
 
-    Assertions.assertEquals(result.id(), t.getId());
-    Assertions.assertEquals(result.title(), t.getTitle());
-    Assertions.assertEquals(result.description(), t.getDescription());
+    Assertions.assertNull(result.getId());
+    Assertions.assertEquals(result.getTitle(), record.title());
+    Assertions.assertEquals(result.getDescription(), record.description());
   }
 }
