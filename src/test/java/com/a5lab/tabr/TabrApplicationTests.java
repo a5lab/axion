003623aa079fbf43db8fe7933e06e5e05c1b7c32
arufanov <arufanov@gmail.com>
2022-12-28 14:55:46 +0300

    add integration tests

--- src/test/java/com/a5lab/tabr/TabrApplicationTests.java
+++ src/test/java/com/a5lab/tabr/TabrApplicationTests.java
@@ -1,16 +1,25 @@
 package com.a5lab.tabr;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
 class TabrApplicationTests {
 
+  @Autowired
+  private MockMvc mockMvc;
+
   @Test
-  void contextLoads() {
-
+  void getHomePage() throws Exception {
+    mockMvc.perform(get("/")).andExpect(status().isOk());
   }
-
 }
