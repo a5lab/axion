b276bee6cc350af1376006b0ed8118231345c6ea
arufanov <arufanov@gmail.com>
2022-12-28 15:57:55 +0300

    add contrller test

---
+++ src/test/java/com/a5lab/tabr/controllers/HomeControllerTest.java
@@ -0,0 +1,22 @@
+package com.a5lab.tabr.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.a5lab.tabr.domain.radars.Radar;
+
+@Controller
+public class HomeController {
+
+  @GetMapping({"/", "home.html"})
+  public ModelAndView index() {
+    Radar radar = new Radar();
+    radar.setTitle("myTitle");
+    radar.setDescription("myDescriptions");
+
+    ModelAndView modelAndView = new ModelAndView("home");
+    modelAndView.addObject(radar);
+    return modelAndView;
+  }
+}
