fcf8e4042a368dc2e04ae749695fa59bc96a4601
arufanov <arufanov@gmail.com>
2023-01-02 20:21:25 +0300

    add integration tests (#121)
    Controller test has been added.

---
+++ src/test/java/com/a5lab/tabr/domain/HomeControllerTests.java
@@ -0,0 +1,25 @@
+package com.a5lab.tabr.domain;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.a5lab.tabr.controllers.HomeController;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTests {
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void index() throws Exception {
+    mockMvc.perform(get("/")).andExpect(status().isOk());
+  }
+}
