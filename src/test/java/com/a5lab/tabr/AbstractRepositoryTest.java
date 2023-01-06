3aa887ed63c1900ed0ff699e92ff4f18a9419dd5
arufanov <arufanov@gmail.com>
2023-01-06 22:31:09 +0300

    I18n flash message at controller (#155)
    I18n cotroller has been created

--- src/test/java/com/a5lab/tabr/AbstractRepositoryTest.java
+++ src/test/java/com/a5lab/tabr/AbstractRepositoryTest.java
@@ -10,6 +10,6 @@ import org.springframework.context.annotation.Import;
 @Import({
     JpaAuditingConfiguration.class
 })
-@TabrTestBaseConfig
+@ApplicationTestBaseConfig
 public abstract class AbstractRepositoryTest {
 }
