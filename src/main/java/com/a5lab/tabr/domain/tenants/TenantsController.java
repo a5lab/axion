2032b3aca105deff5ce5a1eee5b616f8ec652bf3
arufanov <arufanov@gmail.com>
2023-01-07 00:19:08 +0300

    update application

--- src/main/java/com/a5lab/tabr/domain/tenants/TenantsController.java
+++ src/main/java/com/a5lab/tabr/domain/tenants/TenantsController.java
@@ -63,7 +63,7 @@ public class TenantsController {
   public String create(@Valid TenantRecord tenantRecord, BindingResult result,
                        RedirectAttributes redirectAttributes) {
     if (result.hasErrors()) { // Doesn't work
-      return "/settings/tenants/add.html";
+      return "/settings/tenants/add";
     }
     tenantService.save(tenantRecord);
     redirectAttributes.addFlashAttribute(FlashMessages.INFO,
@@ -91,7 +91,7 @@ public class TenantsController {
   public String update(@PathVariable("id") Long id, @Valid TenantRecord tenantRecord,
                        BindingResult result, RedirectAttributes redirectAttributes) {
     if (result.hasErrors()) {
-      return "/settings/tenants/edit.html/{id}";
+      return "/settings/tenants/edit/{id}";
     }
     tenantService.save(tenantRecord); // !!! a new insert? constraint failure
     redirectAttributes.addFlashAttribute(FlashMessages.INFO,
