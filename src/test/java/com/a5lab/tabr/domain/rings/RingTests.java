cebc73d90a214b3a776d5350d6b4e812ae7710d2
arufanov <arufanov@gmail.com>
2023-01-08 22:47:18 +0300

    add tests

--- src/test/java/com/a5lab/tabr/domain/rings/RingTests.java
+++ src/test/java/com/a5lab/tabr/domain/rings/RingTests.java
@@ -3,132 +3,51 @@ package com.a5lab.tabr.domain.rings;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.a5lab.tabr.AbstractRepositoryTests;
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ValidationException;
+import jakarta.validation.Validator;
+import java.util.Set;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-class RingRepositoryTests extends AbstractRepositoryTests {
+class RingTests extends AbstractRepositoryTests {
 
   @Autowired
-  private RingRepository ringRepository;
+  private Validator validator;
 
   @Test
-  void shouldSaveRingWithTitleAndDescr() {
-    final Ring r = new Ring();
-    r.setTitle("TEST");
-    r.setDescription("Very good description for Ring");
-
-    Assertions.assertNull(r.getId());
-    ringRepository.saveAndFlush(r);
-    Assertions.assertNotNull(r.getId());
-    Assertions.assertNotNull(r.getCreatedBy());
-    Assertions.assertNotNull(r.getCreatedDate());
-    Assertions.assertNotNull(r.getLastModifiedBy());
-    Assertions.assertNotNull(r.getLastModifiedDate());
+  void ensureValidatorIsLoaded() {
+    // https://stackoverflow.com/questions/29069956/how-to-test-validation-annotations-of-a-class-using-junit
+    // Should we user hibernate-validator? But what is about dto?
+    Assertions.assertNotNull(validator);
   }
 
   @Test
   void shouldFailOnNullTitle() {
-    final Ring r = new Ring();
-    r.setDescription("Very good description for Ring");
-
-    Assertions.assertNull(r.getId());
-    assertThatThrownBy(() -> ringRepository.saveAndFlush(r))
-        .isInstanceOf(ValidationException.class);
+    final Ring ring = new Ring();
+    Set<ConstraintViolation<Ring>> violations = validator.validate(ring);
+    Assertions.assertFalse(violations.isEmpty());
   }
 
   @Test
-  void shouldFailOnEmptyTitle() {
-    final Ring r = new Ring();
-    r.setTitle("");
-    r.setDescription("Very good description for Ring");
+  void shouldFailOnBlankTitle() {
+    final Ring ring = new Ring();
+    ring.setTitle("");
 
-    Assertions.assertNull(r.getId());
-    assertThatThrownBy(() -> ringRepository.saveAndFlush(r))
-        .isInstanceOf(ValidationException.class);
+    Set<ConstraintViolation<Ring>> violations = validator.validate(ring);
+    Assertions.assertFalse(violations.isEmpty());
   }
 
   @Test
-  void shouldFailOnWhiteSpaceTitle() {
-    final Ring r = new Ring();
-    r.setTitle(" ");
-    r.setDescription("Very good description for Ring");
+  void shouldBeOnOnNotBlankTitle() {
+    final Ring ring = new Ring();
+    ring.setTitle("Title");
+    ring.setDescription("Description");
 
-    Assertions.assertNull(r.getId());
-    assertThatThrownBy(() -> ringRepository.saveAndFlush(r))
-        .isInstanceOf(ValidationException.class);
+    Set<ConstraintViolation<Ring>> violations = validator.validate(ring);
+    Assertions.assertTrue(violations.isEmpty());
   }
 
-  @Test
-  void shouldFailOnNullDescription() {
-    final Ring r = new Ring();
-    r.setTitle("TEST");
-
-    Assertions.assertNull(r.getId());
-    assertThatThrownBy(() -> ringRepository.saveAndFlush(r))
-        .isInstanceOf(ValidationException.class);
-  }
-
-  @Test
-  void shouldFailOnEmptyDescription() {
-    final Ring r = new Ring();
-    r.setTitle("TEST");
-    r.setDescription("");
-
-    Assertions.assertNull(r.getId());
-    assertThatThrownBy(() -> ringRepository.saveAndFlush(r))
-        .isInstanceOf(ValidationException.class);
-  }
-
-  @Test
-  void shouldFailOnWhiteSpaceDescription() {
-    final Ring r = new Ring();
-    r.setTitle("TEST");
-    r.setDescription(" ");
-
-    Assertions.assertNull(r.getId());
-    assertThatThrownBy(() -> ringRepository.saveAndFlush(r))
-        .isInstanceOf(ValidationException.class);
-  }
-
-  @Test
-  void shouldFailOnLowerTitle() {
-    final Ring r = new Ring();
-    r.setTitle("test");
-    r.setDescription("Very good description for Ring");
-
-    Assertions.assertNull(r.getId());
-    assertThatThrownBy(() -> ringRepository.saveAndFlush(r))
-        .isInstanceOf(ValidationException.class);
-  }
-
-  @Test
-  void shouldFindSavedRingById() {
-    final Ring r = new Ring();
-    r.setTitle("MY");
-    r.setDescription("Very good description for Ring");
-
-    Assertions.assertNull(r.getId());
-    ringRepository.saveAndFlush(r);
-    Assertions.assertNotNull(r.getId());
-    var id = r.getId();
-
-    Assertions.assertTrue(ringRepository.findById(id).isPresent());
-  }
-
-  @Test
-  void shouldFindSavedRingByTitle() {
-    String title = "SUPER";
-    final Ring r = new Ring();
-    r.setTitle(title);
-    r.setDescription("Very good description for Ring");
-
-    Assertions.assertNull(r.getId());
-    ringRepository.saveAndFlush(r);
-    Assertions.assertNotNull(r.getId());
-
-    Assertions.assertTrue(ringRepository.findRingByTitle(title).isPresent());
-  }
 
 }
