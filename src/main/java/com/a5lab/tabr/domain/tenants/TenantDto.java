7dac471a6be915a5f7c5a086f3a23ea51de919a1
arufanov <arufanov@gmail.com>
2023-01-11 02:41:43 +0300

    fix errors

--- src/main/java/com/a5lab/tabr/domain/tenants/TenantDto.java
+++ src/main/java/com/a5lab/tabr/domain/tenants/TenantDto.java
@@ -1,7 +1,6 @@
 package com.a5lab.tabr.domain.tenants;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
