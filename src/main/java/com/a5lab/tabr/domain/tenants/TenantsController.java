cfcdf17921d54d8e94199a5a6142e625e25c1d81
arufanov <arufanov@gmail.com>
2023-01-11 14:39:59 +0300

    updte tenants

--- src/main/java/com/a5lab/tabr/domain/tenants/TenantsController.java
+++ src/main/java/com/a5lab/tabr/domain/tenants/TenantsController.java
@@ -9,7 +9,6 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
