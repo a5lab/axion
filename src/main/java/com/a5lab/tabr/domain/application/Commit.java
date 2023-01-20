296827f378146b922c59ce9c458099988b4f2d81
arufanov <arufanov@gmail.com>
2023-01-21 00:31:58 +0300

    remove components

--- src/main/java/com/a5lab/tabr/domain/application/Commit.java
+++ src/main/java/com/a5lab/tabr/domain/application/Commit.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
