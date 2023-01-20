8098a6591c7fcca9dff19d2ac74326c281db7389
arufanov <arufanov@gmail.com>
2023-01-21 00:14:34 +0300

    fix commit

--- src/main/java/com/a5lab/tabr/domain/application/Commit.java
+++ src/main/java/com/a5lab/tabr/domain/application/Commit.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
