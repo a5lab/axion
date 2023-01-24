27e21e4b96ccda3ddc8d257e1722995fc500746e
arufanov <arufanov@gmail.com>
2023-01-24 16:23:24 +0300

    update version

--- src/main/java/com/a5lab/tabr/domain/application/VersionApiController.java
+++ src/main/java/com/a5lab/tabr/domain/application/VersionApiController.java
@@ -1,7 +1,6 @@
 package com.a5lab.tabr.domain.application;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,29 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableConfigurationProperties(Version.class)
 public class VersionApiController {
 
-  @Value("${application.group_id}")
-  private String groupId;
-
-  @Value("${application.artifact_id}")
-  private String artifactId;
-
-  @Value("${application.version}")
-  private String version;
-
-  @Value("${application.name}")
-  private String name;
-
-  @Value("${application.description}")
-  private String description;
+  private final Version version;
 
   @GetMapping("/show")
   public Version show() {
-    Version version = new Version();
-    version.setGroupId(this.groupId);
-    version.setArtifactId(this.artifactId);
-    version.setVersion(this.version);
-    version.setName(this.name);
-    version.setDescription(this.description);
     return version;
   }
 }
