892812c3dc58bca662aa80f6e9ecef2a8159d555
arufanov <arufanov@gmail.com>
2023-01-25 00:17:47 +0300

    rename radars

--- src/main/java/com/a5lab/tabr/domain/radars/DashboardController.java
+++ src/main/java/com/a5lab/tabr/domain/radars/DashboardController.java
@@ -1,4 +1,4 @@
-package com.a5lab.tabr.domain;
+package com.a5lab.tabr.domain.radars;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
