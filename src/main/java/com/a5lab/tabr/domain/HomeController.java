4d11d1aa7a677ae9969c94b41b59c2a93ab89c7b
arufanov <arufanov@gmail.com>
2023-01-24 18:41:41 +0300

    fix radars

--- src/main/java/com/a5lab/tabr/domain/HomeController.java
+++ src/main/java/com/a5lab/tabr/domain/HomeController.java
@@ -1,21 +1,32 @@
 package com.a5lab.tabr.domain;
 
+import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.a5lab.tabr.domain.radars.Radar;
+import com.a5lab.tabr.domain.radars.RadarDto;
 import com.a5lab.tabr.domain.radars.RadarService;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+
   private final RadarService radarService;
 
   @GetMapping({"/", "home"})
   public ModelAndView index() {
 
+    Optional<RadarDto> radarDtoOptional = radarService.findByPrimary(true);
+    ModelAndView modelAndView = new ModelAndView("home");
+    if (radarDtoOptional.isPresent()) {
+      modelAndView.addObject("radarDto", radarDtoOptional.get());
+    }
+    return modelAndView;
+
+    /* todo remove it
     Radar radar = new Radar();
     radar.setTitle("myTitle");
     radar.setDescription("myDescriptions");
@@ -23,5 +34,6 @@ public class HomeController {
     ModelAndView modelAndView = new ModelAndView("home");
     modelAndView.addObject(radar);
     return modelAndView;
+     */
   }
 }
