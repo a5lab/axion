34e1a5e957fa75de0b5c0723767b5fdc1b5a569d
arufanov <arufanov@gmail.com>
2023-01-26 01:00:47 +0300

    update controllers

--- src/main/java/com/a5lab/tabr/domain/radars/RadarsController.java
+++ src/main/java/com/a5lab/tabr/domain/radars/RadarsController.java
@@ -1,115 +1,27 @@
 package com.a5lab.tabr.domain.radars;
 
-import jakarta.validation.Valid;
-import java.util.Optional;
-
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.a5lab.tabr.utils.FlashMessages;
-
 
 @Controller
-@RequestMapping("/settings/radars")
+@RequestMapping("/radars")
 @RequiredArgsConstructor
 public class RadarsController {
 
   private final RadarService radarService;
-  private final MessageSource messageSource;
 
   @GetMapping("")
   public ModelAndView index() {
     // We need to replace it with proper values for PageRequest.of() coming from ui
     // See https://github.com/a5lab/tabr/issues/112
-    ModelAndView modelAndView = new ModelAndView("settings/radars/index");
+    ModelAndView modelAndView = new ModelAndView("radars/index");
     modelAndView.addObject("radars",
         radarService.findAll(Pageable.ofSize(100)).getContent());
     return modelAndView;
   }
 
-  @GetMapping("/show/{id}")
-  public ModelAndView show(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
-    Optional<RadarDto> radarRecord = radarService.findById(id);
-    if (radarRecord.isPresent()) {
-      ModelAndView modelAndView = new ModelAndView("settings/radars/show");
-      modelAndView.addObject("radarDto", radarRecord.get());
-      return modelAndView;
-    } else {
-      redirectAttributes.addFlashAttribute(FlashMessages.ERROR,
-          messageSource.getMessage("radar.flash.error.invalid_id", null,
-              LocaleContextHolder.getLocale()));
-      return new ModelAndView("redirect:/settings/radars");
-    }
-  }
-
-  @GetMapping("/add")
-  public ModelAndView add() {
-    ModelAndView modelAndView = new ModelAndView("settings/radars/add");
-    modelAndView.addObject("radarDto", new RadarDto());
-    return modelAndView;
-  }
-
-  @PostMapping(value = "/create")
-  public ModelAndView create(@Valid RadarDto radarDto, BindingResult bindingResult,
-                       RedirectAttributes redirectAttributes) {
-    if (bindingResult.hasErrors()) {
-      ModelAndView modelAndView = new ModelAndView("settings/radars/add");
-      modelAndView.addObject("radarDto", radarDto);
-      return modelAndView;
-    }
-    radarService.save(radarDto);
-    redirectAttributes.addFlashAttribute(FlashMessages.INFO,
-        messageSource.getMessage("radar.flash.info.created", null,
-            LocaleContextHolder.getLocale()));
-    return new ModelAndView("redirect:/settings/radars");
-  }
-
-  @GetMapping(value = "/edit/{id}")
-  public ModelAndView edit(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
-    Optional<RadarDto> radarDto = radarService.findById(id);
-    if (radarDto.isPresent()) {
-      ModelAndView modelAndView = new ModelAndView("settings/radars/edit");
-      modelAndView.addObject("radarDto", radarDto.get());
-      return modelAndView;
-    } else {
-      redirectAttributes.addFlashAttribute(FlashMessages.ERROR,
-          messageSource.getMessage("radar.flash.error.invalid_id", null,
-              LocaleContextHolder.getLocale()));
-      return new ModelAndView("redirect:/settings/radars");
-    }
-  }
-
-  @PostMapping("/update")
-  public ModelAndView update(@Valid RadarDto radarDto,
-                       BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-    if (bindingResult.hasErrors()) {
-      ModelAndView modelAndView = new ModelAndView("settings/radars/edit");
-      modelAndView.addObject("radarDto", radarDto);
-      return modelAndView;
-    }
-    radarService.save(radarDto);
-    redirectAttributes.addFlashAttribute(FlashMessages.INFO,
-        messageSource.getMessage("radar.flash.info.updated", null,
-            LocaleContextHolder.getLocale()));
-    return new ModelAndView("redirect:/settings/radars");
-  }
-
-  @GetMapping(value = "/delete/{id}")
-  public String delete(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
-    radarService.deleteById(id);
-    redirectAttributes.addFlashAttribute(FlashMessages.INFO,
-        messageSource.getMessage("radar.flash.info.deleted", null,
-            LocaleContextHolder.getLocale()));
-    return "redirect:/settings/radars";
-  }
 }
