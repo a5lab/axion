6b19734d875a0f4b6f803a3497e84c0cca4dcea5
arufanov <arufanov@gmail.com>
2023-01-25 14:16:24 +0300

    impelemneted pageing\

--- src/main/java/com/a5lab/tabr/domain/entries/EntriesController.java
+++ src/main/java/com/a5lab/tabr/domain/entries/EntriesController.java
@@ -1,18 +1,23 @@
 package com.a5lab.tabr.domain.entries;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -28,12 +33,20 @@ public class EntriesController {
   private final MessageSource messageSource;
 
   @GetMapping("")
-  public ModelAndView index() {
+  public ModelAndView index(@RequestParam("page") Optional<Integer> page,
+                            @RequestParam("size") Optional<Integer> size) {
+    int currentPage = page.orElse(1);
+    int pageSize = size.orElse(10);
     ModelAndView modelAndView = new ModelAndView("settings/entries/index");
-    // We need to replace it with proper values for PageRequest.of() coming from ui
-    // See https://github.com/a5lab/tabr/issues/112
-    modelAndView.addObject("entries",
-        entryService.findAll(Pageable.ofSize(100)).getContent());
+    Page<EntryDto> entryDtoPage = entryService.findAll(PageRequest.of(currentPage - 1, pageSize));
+    modelAndView.addObject("entryDtoPage", entryDtoPage);
+
+    int totalPages = entryDtoPage.getTotalPages();
+    if (totalPages > 0) {
+      List<Integer> pageNumbers =
+          IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+      modelAndView.addObject("pageNumbers", pageNumbers);
+    }
     return modelAndView;
   }
 
@@ -91,7 +104,7 @@ public class EntriesController {
 
   @PostMapping("/update")
   public ModelAndView update(@Valid EntryDto entryDto,
-                       BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+                             BindingResult bindingResult, RedirectAttributes redirectAttributes) {
     if (bindingResult.hasErrors()) {
       ModelAndView modelAndView = new ModelAndView("settings/entries/edit");
       modelAndView.addObject("entryDto", entryDto);
