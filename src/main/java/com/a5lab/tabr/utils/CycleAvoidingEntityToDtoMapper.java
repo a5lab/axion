ef748d5749be7a3bb3f95d35d5a9684f40d8fd66
Yuriy Chervonyi <zolotoych@yandex.ru>
2023-02-01 22:20:39 +0300

    Fixed Cycling

--- src/main/java/com/a5lab/tabr/utils/CycleAvoidingEntityToDtoMapper.java
+++ src/main/java/com/a5lab/tabr/utils/CycleAvoidingEntityToDtoMapper.java
@@ -1,12 +1,9 @@
 package com.a5lab.tabr.utils;
 
 import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.commons.collections4.map.AbstractReferenceMap;
 import org.apache.commons.collections4.map.ReferenceIdentityMap;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.BeforeMapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.TargetType;
@@ -14,10 +11,6 @@ import org.mapstruct.TargetType;
 public abstract class CycleAvoidingEntityToDtoMapper<E, D> implements EntityToDtoMapper<E, D> {
 
   private static final Map<Object, Object> KNOWN_INSTANCES =
-      new ReferenceIdentityMap<>(
-          AbstractReferenceMap.ReferenceStrength.WEAK, AbstractReferenceMap.ReferenceStrength.WEAK,
-          true);
-  private static final Map<Object, AtomicLong> TRACE_COUNT =
       new ReferenceIdentityMap<>(
           AbstractReferenceMap.ReferenceStrength.WEAK, AbstractReferenceMap.ReferenceStrength.WEAK,
           true);
@@ -30,20 +23,6 @@ public abstract class CycleAvoidingEntityToDtoMapper<E, D> implements EntityToDt
   @BeforeMapping
   protected void storeMappedInstance(E source, @MappingTarget D target) {
     KNOWN_INSTANCES.putIfAbsent(source, target);
-    TRACE_COUNT.putIfAbsent(source, new AtomicLong(0));
-    TRACE_COUNT.get(source).incrementAndGet();
-  }
-
-  @AfterMapping
-  protected void cleanUp(E source) {
-    Optional.ofNullable(TRACE_COUNT.get(source))
-        .map(AtomicLong::decrementAndGet)
-        .ifPresent(it -> {
-          if (it <= 0) {
-            KNOWN_INSTANCES.remove(source);
-            TRACE_COUNT.remove(source);
-          }
-        });
   }
 
 }
