b75ecc5b22865dd83893bfd71e80e81992b893ed
arufanov <arufanov@gmail.com>
2023-02-13 01:23:20 +0300

    Package coding style (#273)
    Coding style has been fixed (no s at package and controller)

---
+++ src/main/java/com/a5lab/axion/domain/radar/Radar.java
@@ -0,0 +1,69 @@
+package com.a5lab.axion.domain.radar;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.DynamicUpdate;
+
+import com.a5lab.axion.domain.AbstractAuditable;
+import com.a5lab.axion.domain.blip.Blip;
+import com.a5lab.axion.domain.radar_type.RadarType;
+import com.a5lab.axion.utils.JpaConstants;
+
+@Entity
+@Table(name = "radars")
+@DynamicUpdate
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@RadarPrimaryConstraint
+public class Radar extends AbstractAuditable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, updatable = false, unique = true)
+  private Long id;
+
+  @NotNull
+  @ManyToOne(optional = false, fetch = FetchType.EAGER)
+  @JoinColumn(name = "radar_type_id", nullable = false, updatable = false)
+  private RadarType radarType;
+
+  @NotBlank
+  @Column(name = "title", unique = true, nullable = false)
+  private String title;
+
+  @NotBlank
+  @Column(name = "description", nullable = false)
+  private String description;
+
+  @Column(name = "is_primary", nullable = false)
+  private boolean primary = true;
+
+  @Column(name = "is_active", nullable = false)
+  private boolean active = true;
+
+  @Setter(AccessLevel.NONE)
+  @OneToMany(fetch = FetchType.EAGER, mappedBy = "radar")
+  @BatchSize(size = JpaConstants.BATCH_SIZE_FOR_COLLECTIONS)
+  private List<Blip> blipList;
+}
---
+++ src/main/java/com/a5lab/axion/domain/ring/RingService.java
@@ -0,0 +1,22 @@
+package com.a5lab.axion.domain.ring;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface RingService {
+
+  Collection<RingDto> findAll();
+
+  Page<RingDto> findAll(Pageable pageable);
+
+  Optional<RingDto> findById(Long id);
+
+  Optional<Ring> findByTitle(String title);
+
+  RingDto save(RingDto ringDto);
+
+  void deleteById(Long id);
+}
