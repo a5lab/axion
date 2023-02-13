cb3f0bb7e56e7d30037dedaba5b61f74911f2f8e
arufanov <arufanov@gmail.com>
2023-02-13 23:02:30 +0300

    intermediat

--- src/main/java/com/a5lab/axion/domain/radar/RadarController.java
+++ src/main/java/com/a5lab/axion/domain/radar/RadarController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/radar")
+@RequestMapping("/radars")
 @RequiredArgsConstructor
 public class RadarController {
 
