eb6318a9ef4f69167f781f4ff4dca225701dfc6f
arufanov <arufanov@gmail.com>
2023-02-14 17:04:33 +0300

    fix tests

--- src/test/java/com/a5lab/axion/domain/radar/RadarRepositoryTests.java
+++ src/test/java/com/a5lab/axion/domain/radar/RadarRepositoryTests.java
@@ -1,12 +1,16 @@
 package com.a5lab.axion.domain.radar;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowableOfType;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.ValidationException;
 
 
 import com.a5lab.axion.domain.radar_type.RadarType;
 import com.a5lab.axion.domain.radar_type.RadarTypeRepository;
+import org.hibernate.validator.internal.engine.path.PathImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +39,35 @@ class RadarRepositoryTests extends AbstractRepositoryTests {
     radar.setTitle("My new test Radar");
     radar.setDescription("My awesome description");
     radar.setPrimary(false);
+    radar.setActive(false);
+
+    Assertions.assertNull(radar.getId());
+    radarRepository.saveAndFlush(radar);
+    Assertions.assertNotNull(radar.getId());
+    Assertions.assertNotNull(radar.getRadarType());
+    Assertions.assertNotNull(radar.getTitle());
+    Assertions.assertNotNull(radar.getDescription());
+    Assertions.assertNotNull(radar.getCreatedBy());
+    Assertions.assertNotNull(radar.getCreatedDate());
+    Assertions.assertNotNull(radar.getLastModifiedBy());
+    Assertions.assertNotNull(radar.getLastModifiedDate());
+  }
+
+  @Test
+  void shouldSavePrimaryRadarWithAllFields() {
+    // Create a radar type
+    final RadarType radarType = new RadarType();
+    radarType.setTitle("Technology radars");
+    radarType.setCode("technology_radar");
+    radarType.setDescription("Technology radars");
+    radarTypeRepository.saveAndFlush(radarType);
+
+    // Create a radar
+    final Radar radar = new Radar();
+    radar.setRadarType(radarType);
+    radar.setTitle("My new test Radar");
+    radar.setDescription("My awesome description");
+    radar.setPrimary(true);
     radar.setActive(false);
 
     Assertions.assertNull(radar.getId());
@@ -111,6 +144,47 @@ class RadarRepositoryTests extends AbstractRepositoryTests {
     Assertions.assertNull(radar.getId());
     assertThatThrownBy(() -> radarRepository.saveAndFlush(radar))
         .isInstanceOf(ValidationException.class);
+  }
+
+  @Test
+  void shouldFailOnSecondPrimaryRadar() {
+    // Create a radar type
+    final RadarType radarType = new RadarType();
+    radarType.setTitle("Technology radars");
+    radarType.setCode("technology_radar");
+    radarType.setDescription("Technology radars");
+    radarTypeRepository.saveAndFlush(radarType);
+
+    // Create a radar
+    final Radar radar = new Radar();
+    radar.setRadarType(radarType);
+    radar.setTitle("My new test radar");
+    radar.setDescription("My awesome description");
+    radar.setPrimary(true);
+    radar.setActive(false);
+    radarRepository.saveAndFlush(radar);
+
+    // Create second radar
+    final Radar radar1 = new Radar();
+    radar1.setRadarType(radarType);
+    radar1.setTitle("My new primary test radar");
+    radar1.setDescription("My awesome description");
+    radar1.setPrimary(true);
+    radar1.setActive(false);
+
+    Assertions.assertNull(radar1.getId());
+    ConstraintViolationException exception =
+        catchThrowableOfType(() -> radarRepository.saveAndFlush(radar1),
+            ConstraintViolationException.class);
+
+    Assertions.assertNotNull(exception);
+    Assertions.assertEquals(exception.getConstraintViolations().size(), 1);
+    for (ConstraintViolation<?> constraintViolation : exception.getConstraintViolations()) {
+      Assertions.assertEquals(
+          ((PathImpl) constraintViolation.getPropertyPath()).getLeafNode().asString(), "primary");
+      Assertions.assertEquals(constraintViolation.getMessage(), "can be only one primary");
+    }
+
   }
 
 }
