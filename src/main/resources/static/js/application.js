ac2177febf62dd997a18c1c6bcb13c765e981ca8
arufanov <arufanov@gmail.com>
2023-02-18 23:32:05 +0300

    udpate filter

--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -1,3 +1,14 @@
+/*
+ * Helper that show/hide filter.
+ * Helper assumes only one filter per page.
+ */
+$(document).ready(function () {
+    $("#filter-nav").click(function (event) {
+        $("#filter-form").toggle();
+    });
+});
+
+/* todo: remove this code
 document.querySelectorAll('.js-submit-confirm').forEach(($item) => {
     $item.addEventListener('submit', (event) => {
         if (!confirm(event.currentTarget.getAttribute('data-confirm-message'))) {
@@ -7,3 +18,4 @@ document.querySelectorAll('.js-submit-confirm').forEach(($item) => {
         return true;
     });
 });
+*/
