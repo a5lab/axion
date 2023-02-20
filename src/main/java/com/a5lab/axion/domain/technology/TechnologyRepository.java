d87efd010705ee350adbede5fef10d8d299032a3
arufanov <arufanov@gmail.com>
2023-02-20 23:53:18 +0300

    update filters

--- src/main/java/com/a5lab/axion/domain/technology/TechnologyRepository.java
+++ src/main/java/com/a5lab/axion/domain/technology/TechnologyRepository.java
@@ -3,9 +3,11 @@ package com.a5lab.axion.domain.technology;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TechnologyRepository extends JpaRepository<Technology, Long> {
+public interface TechnologyRepository extends JpaRepository<Technology, Long>,
+    JpaSpecificationExecutor<Technology> {
   Optional<Technology> findByTitle(String title);
 }
