b0e0411127388c3a77aa10094b405cb543dfd1a9
VR <vladimir.n.romanov@gmail.com>
2023-02-22 19:52:11 +0300

    'RadarPrimaryValidator' instantiation fixed (#306)
    * 'RadarPrimaryValidator' instantiation fixed
    
    * 'RadarPrimaryValidator' instantiation fixed
    
    ---------
    
    Co-authored-by: vladimir romanov <vnromanov@inno.tech>

---
+++ src/main/java/com/a5lab/axion/domain/radar/ApplicationContextProvider.java
@@ -0,0 +1,19 @@
+package com.a5lab.axion.domain.radar;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextProvider implements ApplicationContextAware{
+    private static ApplicationContext context;
+    public static ApplicationContext getApplicationContext() {
+        return context;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        this.context = context;
+    }
+}
--- src/main/java/com/a5lab/axion/domain/radar/RadarPrimaryValidator.java
+++ src/main/java/com/a5lab/axion/domain/radar/RadarPrimaryValidator.java
@@ -3,13 +3,15 @@ package com.a5lab.axion.domain.radar;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 public class RadarPrimaryValidator
     implements ConstraintValidator<RadarPrimaryConstraint, Radar> {
 
-  private final RadarService radarService;
+  public RadarPrimaryValidator() {
+    radarService = ApplicationContextProvider
+            .getApplicationContext().getBean(RadarService.class);
+  }
+
+  private RadarService radarService;
   // private final RadarRepository radarRepository;
 
   @Override
