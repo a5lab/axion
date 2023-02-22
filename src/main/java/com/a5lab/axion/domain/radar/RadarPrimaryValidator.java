d6c700ec0e557155e95e77106fcc8b1d3916cf86
vladimir romanov <vnromanov@inno.tech>
2023-02-22 19:11:26 +0300

    'RadarPrimaryValidator' instantiation fixed

--- src/main/java/com/a5lab/axion/domain/radar/RadarPrimaryValidator.java
+++ src/main/java/com/a5lab/axion/domain/radar/RadarPrimaryValidator.java
@@ -2,28 +2,20 @@ package com.a5lab.axion.domain.radar;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.Assert;
 
 public class RadarPrimaryValidator
     implements ConstraintValidator<RadarPrimaryConstraint, Radar> {
+
   public RadarPrimaryValidator() {
     radarService = ApplicationContextProvider
             .getApplicationContext().getBean(RadarService.class);
   }
 
   private RadarService radarService;
-  @Autowired
-  public void setRadarService(RadarService radarService) {
-    this.radarService = radarService;
-  }
-
   // private final RadarRepository radarRepository;
 
   @Override
   public boolean isValid(Radar radar, ConstraintValidatorContext context) {
-
-    Assert.notNull(radarService);
     //
     return true;
     /*
