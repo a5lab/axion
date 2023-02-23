54b1a837b215d9f6b18c39ca05f3bd69b75f975f
vladimir romanov <vnromanov@inno.tech>
2023-02-23 19:10:31 +0300

    'RadarPrimaryValidator' instantiation fixed

--- src/test/java/com/a5lab/axion/domain/AbstractRepositoryTests.java
+++ src/test/java/com/a5lab/axion/domain/AbstractRepositoryTests.java
@@ -1,11 +1,12 @@
 package com.a5lab.axion.domain;
 
-import com.a5lab.axion.AxionApplication;
-import com.a5lab.axion.config.ApplicationTestBaseConfig;
-import com.a5lab.axion.config.JpaAuditingConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+
+import com.a5lab.axion.AxionApplication;
+import com.a5lab.axion.config.ApplicationTestBaseConfig;
+import com.a5lab.axion.config.JpaAuditingConfiguration;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
