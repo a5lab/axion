ee82781b1c45ca9440b1111706b55d55468a1956
arufanov <arufanov@gmail.com>
2023-02-24 23:24:46 +0300

    update profile

--- src/main/java/com/a5lab/axion/domain/exception/ExceptionController.java
+++ src/main/java/com/a5lab/axion/domain/exception/ExceptionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@Profile("prod")
+@Profile("dev")
 public class ExceptionController implements ErrorController {
 
   @RequestMapping("/error")
