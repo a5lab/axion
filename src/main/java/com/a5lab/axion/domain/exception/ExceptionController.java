23fe12f71fdc58c691bdfd22bf0cca15b0176e93
arufanov <arufanov@gmail.com>
2023-02-25 00:22:19 +0300

    fix erors

--- src/main/java/com/a5lab/axion/domain/exception/ExceptionController.java
+++ src/main/java/com/a5lab/axion/domain/exception/ExceptionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@Profile("dev")
+@Profile("prod")
 public class ExceptionController implements ErrorController {
 
   @RequestMapping("/error")
