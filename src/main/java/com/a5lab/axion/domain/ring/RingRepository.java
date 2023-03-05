4bec0172780d557bb48173d9e926f494584ddaea
arufanov <arufanov@gmail.com>
2023-03-05 15:53:22 +0300

    update rings filtering

--- src/main/java/com/a5lab/axion/domain/ring/RingRepository.java
+++ src/main/java/com/a5lab/axion/domain/ring/RingRepository.java
@@ -3,9 +3,12 @@ package com.a5lab.axion.domain.ring;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface RingRepository extends JpaRepository<Ring, Long> {
+public interface RingRepository extends JpaRepository<Ring, Long>,
+    JpaSpecificationExecutor<Ring> {
   Optional<Ring> findByTitle(String title);
 }
