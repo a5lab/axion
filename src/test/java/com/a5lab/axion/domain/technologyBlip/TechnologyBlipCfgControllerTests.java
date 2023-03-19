729ccae12467019421ac7f7e2cfd3c2efc272ebc
Pavel <pavel.agf@gmail.com>
2023-03-19 12:34:16 +0300

    tests failed

--- src/test/java/com/a5lab/axion/domain/technologyBlip/TechnologyBlipCfgControllerTests.java
+++ src/test/java/com/a5lab/axion/domain/technologyBlip/TechnologyBlipCfgControllerTests.java
@@ -2,6 +2,15 @@ package com.a5lab.axion.domain.technologyBlip;
 
 import com.a5lab.axion.domain.AbstractControllerTests;
 import com.a5lab.axion.domain.radar.Radar;
+import com.a5lab.axion.domain.radar.RadarService;
+import com.a5lab.axion.domain.ring.RingDto;
+import com.a5lab.axion.domain.ring.RingService;
+import com.a5lab.axion.domain.segment.Segment;
+import com.a5lab.axion.domain.segment.SegmentDto;
+import com.a5lab.axion.domain.segment.SegmentService;
+import com.a5lab.axion.domain.technology.Technology;
+import com.a5lab.axion.domain.technology.TechnologyDto;
+import com.a5lab.axion.domain.technology.TechnologyService;
 import com.a5lab.axion.domain.technology_blip.TechnologyBlipCfgController;
 import com.a5lab.axion.domain.technology_blip.TechnologyBlipDto;
 import com.a5lab.axion.domain.technology_blip.TechnologyBlipService;
@@ -28,14 +37,55 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class TechnologyBlipCfgControllerTests extends AbstractControllerTests {
   @MockBean
   private TechnologyBlipService technologyBlipService;
+  @MockBean
+  private  RadarService radarService;
+  @MockBean
+  private  TechnologyService technologyService;
+  @MockBean
+  private  SegmentService segmentService;
+  @MockBean
+  private  RingService ringService;
 
   @Test
   public void shouldGetTechnologyBlips() throws Exception {
-    final TechnologyBlipDto technologyBlipDto = new TechnologyBlipDto(10L, "My title", "My description");
 
     final Radar radar = new Radar();
+    radar.setId(10L);
     radar.setTitle("My radar");
     radar.setDescription("My radar description");
+
+    final TechnologyDto technologyDto = new TechnologyDto();
+    technologyDto.setId(10L);
+    technologyDto.setTitle("My technology");
+    technologyDto.setWebsite("My website");
+    technologyDto.setDescription("My radar description");
+    technologyDto.setMoved(1);
+    technologyDto.setActive(true);
+
+    final SegmentDto segmentDto = new SegmentDto();
+    segmentDto.setId(10L);
+    segmentDto.setTitle("My segment title");
+    segmentDto.setDescription("My segment description");
+    segmentDto.setRadar(radar);
+    segmentDto.setPosition(1);
+    segmentDto.setActive(true);
+
+    final RingDto ringDto = new RingDto();
+    ringDto.setRadar(radar);
+    ringDto.setTitle("My ring title");
+    ringDto.setDescription("My ring description");
+    ringDto.setPosition(0);
+    ringDto.setActive(true);
+
+    final TechnologyBlipDto technologyBlipDto = new TechnologyBlipDto();
+    technologyBlipDto.setId(10L);
+    technologyBlipDto.setRadar(radar);
+    technologyBlipDto.setTechnology(technologyDto);
+    technologyBlipDto.setSegment(segmentDto);
+    technologyBlipDto.setRing(ringDto);
+
+
+
 
     Page<TechnologyBlipDto> page = new PageImpl<>(List.of(technologyBlipDto));
     Mockito.when(technologyBlipService.findAll(any(), any())).thenReturn(page);
@@ -49,7 +99,9 @@ public class TechnologyBlipCfgControllerTests extends AbstractControllerTests {
     String content = result.getResponse().getContentAsString();
 
     Assertions.assertTrue(content.contains(technologyBlipDto.getRadar()));
-    Assertions.assertTrue(content.contains(technologyBlipDto.getSegment()));
+    Assertions.assertTrue(content.contains(technologyBlipDto.getTechnology()));
+
+
   }
   /*
   @Test
