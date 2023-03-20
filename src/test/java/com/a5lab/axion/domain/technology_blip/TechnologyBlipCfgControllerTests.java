bafbfaa4f0099a157d72e4dd7b99642a7f8331d0
A9afon <121198723+A9afon@users.noreply.github.com>
2023-03-20 12:46:48 +0300

    Create tests for techonologyblip (#368)
    The test shouldGetTechnologyBlips has been added.
    ---------
    
    Co-authored-by: arufanov <arufanov@gmail.com>

---
+++ src/test/java/com/a5lab/axion/domain/technology_blip/TechnologyBlipCfgControllerTests.java
@@ -0,0 +1,232 @@
+package com.a5lab.axion.domain.technology_blip;
+
+import com.a5lab.axion.domain.AbstractControllerTests;
+import com.a5lab.axion.domain.radar.Radar;
+import com.a5lab.axion.domain.radar.RadarService;
+import com.a5lab.axion.domain.ring.RingDto;
+import com.a5lab.axion.domain.ring.RingService;
+import com.a5lab.axion.domain.segment.SegmentDto;
+import com.a5lab.axion.domain.segment.SegmentService;
+import com.a5lab.axion.domain.technology.TechnologyDto;
+import com.a5lab.axion.domain.technology.TechnologyService;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(TechnologyBlipCfgController.class)
+
+public class TechnologyBlipCfgControllerTests extends AbstractControllerTests {
+  @MockBean
+  private TechnologyBlipService technologyBlipService;
+  @MockBean
+  private RadarService radarService;
+  @MockBean
+  private TechnologyService technologyService;
+  @MockBean
+  private SegmentService segmentService;
+  @MockBean
+  private RingService ringService;
+
+  @Test
+  public void shouldGetTechnologyBlips() throws Exception {
+
+    final Radar radar = new Radar();
+    radar.setId(10L);
+    radar.setTitle("My radar");
+    radar.setDescription("My radar description");
+
+    final TechnologyDto technologyDto = new TechnologyDto();
+    technologyDto.setId(10L);
+    technologyDto.setTitle("My technology");
+    technologyDto.setWebsite("My website");
+    technologyDto.setDescription("My radar description");
+    technologyDto.setMoved(1);
+    technologyDto.setActive(true);
+
+    final SegmentDto segmentDto = new SegmentDto();
+    segmentDto.setId(10L);
+    segmentDto.setTitle("My segment title");
+    segmentDto.setDescription("My segment description");
+    segmentDto.setRadar(radar);
+    segmentDto.setPosition(1);
+    segmentDto.setActive(true);
+
+    final RingDto ringDto = new RingDto();
+    ringDto.setRadar(radar);
+    ringDto.setTitle("My ring title");
+    ringDto.setDescription("My ring description");
+    ringDto.setPosition(0);
+    ringDto.setActive(true);
+
+    final TechnologyBlipDto technologyBlipDto = new TechnologyBlipDto();
+    technologyBlipDto.setId(10L);
+    technologyBlipDto.setRadar(radar);
+    technologyBlipDto.setTechnology(technologyDto);
+    technologyBlipDto.setSegment(segmentDto);
+    technologyBlipDto.setRing(ringDto);
+
+    Page<TechnologyBlipDto> page = new PageImpl<>(List.of(technologyBlipDto));
+    Mockito.when(technologyBlipService.findAll(any(), any())).thenReturn(page);
+
+    MvcResult result = mockMvc.perform(get("/settings/technology_blips"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("settings/technology_blips/index"))
+        .andExpect(model().attributeExists("technologyBlipDtoPage"))
+        .andExpect(model().attributeExists("pageNumbers"))
+        .andReturn();
+    String content = result.getResponse().getContentAsString();
+
+    Assertions.assertTrue(content.contains(technologyBlipDto.getRadar().getTitle()));
+    Assertions.assertTrue(content.contains(technologyBlipDto.getTechnology().getTitle()));
+    Assertions.assertTrue(content.contains(technologyBlipDto.getSegment().getTitle()));
+    Assertions.assertTrue(content.contains(technologyBlipDto.getRing().getTitle()));
+  }
+
+  /*
+  @Test
+  public void shouldShowTenant() throws Exception {
+    final TenantDto tenantDto = new TenantDto(10L, "my title", "my description");
+    Mockito.when(tenantService.findById(tenantDto.getId())).thenReturn(Optional.of(tenantDto));
+
+    String url = String.format("/settings/tenants/show/%d", tenantDto.getId());
+    MvcResult result = mockMvc.perform(get(url))
+        .andExpect(status().isOk())
+        .andReturn();
+    String content = result.getResponse().getContentAsString();
+
+    Assertions.assertTrue(content.contains(tenantDto.getTitle()));
+    Assertions.assertTrue(content.contains(tenantDto.getDescription()));
+  }
+
+  @Test
+  public void shouldRedirectShowTenant() throws Exception {
+    MvcResult result = mockMvc.perform(get("/settings/tenants/show/1"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/settings/tenants"))
+        .andReturn();
+  }
+
+  @Test
+  public void shouldAddTenant() throws Exception {
+    MvcResult result = mockMvc.perform(get("/settings/tenants/add"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("settings/tenants/add"))
+        .andExpect(model().attributeExists("tenantDto"))
+        .andReturn();
+    String content = result.getResponse().getContentAsString();
+
+    Assertions.assertTrue(content.contains("title"));
+    Assertions.assertTrue(content.contains("description"));
+  }
+
+  @Test
+  public void shouldCreateTenant() throws Exception {
+    final TenantDto tenantDto = new TenantDto(10L, "my title", "my description");
+
+    MvcResult result = mockMvc.perform(post("/settings/tenants/create")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("title", tenantDto.getTitle())
+            .param("description", tenantDto.getDescription())
+            .sessionAttr("tenantDto", tenantDto))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/settings/tenants"))
+        .andReturn();
+
+    String content = result.getResponse().getContentAsString();
+  }
+
+  @Test
+  public void shouldFailToCreateTenant() throws Exception {
+    final TenantDto tenantDto = new TenantDto(10L, "my title", "my description");
+
+    MvcResult result = mockMvc.perform(post("/settings/tenants/create")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("title", tenantDto.getTitle())
+            .sessionAttr("tenantDto", tenantDto))
+        .andExpect(status().isOk())
+        .andReturn();
+
+    String content = result.getResponse().getContentAsString();
+    Assertions.assertTrue(content.contains("must not be blank"));
+  }
+
+  @Test
+  public void shouldEditTenant() throws Exception {
+    final TenantDto tenantDto = new TenantDto(10L, "my title", "my description");
+    Mockito.when(tenantService.findById(tenantDto.getId())).thenReturn(Optional.of(tenantDto));
+
+    String url = String.format("/settings/tenants/edit/%d", tenantDto.getId());
+    MvcResult result = mockMvc.perform(get(url))
+        .andExpect(status().isOk())
+        .andReturn();
+    String content = result.getResponse().getContentAsString();
+
+    Assertions.assertTrue(content.contains(tenantDto.getTitle()));
+    Assertions.assertTrue(content.contains(tenantDto.getDescription()));
+  }
+
+  @Test
+  public void shouldRedirectEditTenant() throws Exception {
+    MvcResult result = mockMvc.perform(get("/settings/tenants/edit/1"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/settings/tenants"))
+        .andReturn();
+  }
+
+  @Test
+  public void shouldUpdateTenant() throws Exception {
+    final TenantDto tenantDto = new TenantDto(10L, "my title", "my description");
+
+    MvcResult result = mockMvc.perform(post("/settings/tenants/update")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("title", tenantDto.getTitle())
+            .param("description", tenantDto.getDescription())
+            .sessionAttr("tenantDto", tenantDto))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/settings/tenants"))
+        .andReturn();
+
+    String content = result.getResponse().getContentAsString();
+  }
+
+  @Test
+  public void shouldFailToUpdateTenant() throws Exception {
+    final TenantDto tenantDto = new TenantDto(10L, "my title", "my description");
+
+    MvcResult result = mockMvc.perform(post("/settings/tenants/update")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("title", tenantDto.getTitle())
+            .sessionAttr("tenantDto", tenantDto))
+        .andExpect(status().isOk())
+        .andReturn();
+
+    String content = result.getResponse().getContentAsString();
+    Assertions.assertTrue(content.contains("must not be blank"));
+  }
+
+  @Test
+  public void shouldDeleteTenant() throws Exception {
+    final TenantDto tenantDto = new TenantDto(10L, "my title", "my description");
+
+    String url = String.format("/settings/tenants/delete/%d", tenantDto.getId());
+    MvcResult result = mockMvc.perform(get(url))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/settings/tenants"))
+        .andReturn();
+  }
+
+   */
+}
