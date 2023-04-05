d732b79c156132fda690f89a5a0b45931f00ccfa
A9afon <121198723+A9afon@users.noreply.github.com>
2023-04-05 16:27:00 +0300

    Create tests for ring (#429)
    Ring tests have been crearted.
    
    ---------
    
    Co-authored-by: arufanov <arufanov@gmail.com>

--- src/test/java/com/a5lab/axion/domain/ring/RingCfgControllerTests.java
+++ src/test/java/com/a5lab/axion/domain/ring/RingCfgControllerTests.java
@@ -24,120 +24,147 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 
 import com.a5lab.axion.domain.AbstractControllerTests;
+import com.a5lab.axion.domain.radar_type.RadarType;
 
 @WebMvcTest(RingCfgController.class)
 public class RingCfgControllerTests extends AbstractControllerTests {
-    @MockBean
-    private RingService ringService;
+  @MockBean
+  private RingService ringService;
 
-    @MockBean
-    private RadarService radarService;
+  @MockBean
+  private RadarService radarService;
 
-    @Test
-    public void shouldGetRings() throws Exception {
-        // Create radar
-        final Radar radar = new Radar();
-        radar.setTitle("My radar");
-        radar.setDescription("My radar description");
-
-        // Create ring for radar
-        final RingDto ringDto = new RingDto();
-        ringDto.setRadar(radar);
-        ringDto.setTitle("My ring");
-        ringDto.setDescription("My ring description");
-        ringDto.setPosition(0);
-        ringDto.setActive(true);
-
-        List<RingDto> ringList = List.of(ringDto);
-        Page<RingDto> page = new PageImpl<>(ringList);
-        Mockito.when(ringService.findAll(any(), any())).thenReturn(page);
-
-        MvcResult result = mockMvc.perform(get("/settings/rings"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("settings/rings/index"))
-                .andExpect(model().attributeExists("ringDtoPage"))
-                .andExpect(model().attributeExists("pageNumbers"))
-                .andReturn();
-
-        String content = result.getResponse().getContentAsString();
-        Assertions.assertTrue(content.contains(ringDto.getTitle()));
-        Assertions.assertTrue(content.contains(ringDto.getDescription()));
-    }
-
-    @Test
-    public void shouldShowRing() throws Exception {
-        final RingDto ringDto = new RingDto();
-        ringDto.setId(10L);
-        ringDto.setRadar(null);
-        ringDto.setTitle("My ring");
-        ringDto.setDescription("My ring description");
-        ringDto.setColor("#fbdb84");
-        ringDto.setPosition(0);
-        ringDto.setActive(true);
-        Mockito.when(ringService.findById(ringDto.getId())).thenReturn(Optional.of(ringDto));
-
-        String url = String.format("/settings/rings/show/%d", ringDto.getId());
-        MvcResult result = mockMvc.perform(get(url))
-                .andExpect(status().isOk())
-                .andReturn();
-        String content = result.getResponse().getContentAsString();
-
-        Assertions.assertTrue(content.contains(ringDto.getTitle()));
-        Assertions.assertTrue(content.contains(ringDto.getDescription()));
-    }
-
-    @Test
-    public void shouldRedirectShowRing() throws Exception {
-        MvcResult result = mockMvc.perform(get("/settings/rings/show/1"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/settings/rings"))
-                .andReturn();
-    }
-
-    @Test
-    public void shouldAddRing() throws Exception {
-        MvcResult result = mockMvc.perform(get("/settings/rings/add"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("settings/rings/add"))
-                .andExpect(model().attributeExists("ringDto"))
-                .andReturn();
-        String content = result.getResponse().getContentAsString();
-
-        Assertions.assertTrue(content.contains("title"));
-        Assertions.assertTrue(content.contains("description"));
-    }
-
-    @Test
-    public void shouldFailToCreateRingOnLowerCaseTitle() throws Exception {
-        final RingDto ringDto = new RingDto();
-        ringDto.setId(10L);
-        ringDto.setRadar(null);
-        ringDto.setTitle("My ring");
-        ringDto.setDescription("My ring description");
-        ringDto.setColor("#fbdb84");
-        ringDto.setPosition(0);
-        ringDto.setActive(true);
-
-        MvcResult result = mockMvc.perform(post("/settings/rings/create")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .param("description", ringDto.getDescription())
-                        .param("title", ringDto.getTitle())
-                        .sessionAttr("ringDto", ringDto))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        String content = result.getResponse().getContentAsString();
-        Assertions.assertTrue(content.contains("should be uppercase"));
-    }
-  /*
   @Test
-  public void shouldCreateRing() throws Exception {
-    final RingDto ringDto = new RingDto(10L, null, "MY TITLE", "MY DESCRIPTION", 1, true);
+  public void shouldGetRings() throws Exception {
+    // Create radar
+    final Radar radar = new Radar();
+    radar.setTitle("My radar");
+    radar.setDescription("My radar description");
+
+    // Create ring for radar
+    final RingDto ringDto = new RingDto();
+    ringDto.setRadar(radar);
+    ringDto.setTitle("My ring");
+    ringDto.setDescription("My ring description");
+    ringDto.setPosition(0);
+    ringDto.setActive(true);
+
+    List<RingDto> ringList = List.of(ringDto);
+    Page<RingDto> page = new PageImpl<>(ringList);
+    Mockito.when(ringService.findAll(any(), any())).thenReturn(page);
+
+    MvcResult result = mockMvc.perform(get("/settings/rings"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("settings/rings/index"))
+        .andExpect(model().attributeExists("ringDtoPage"))
+        .andExpect(model().attributeExists("pageNumbers"))
+        .andReturn();
+
+    String content = result.getResponse().getContentAsString();
+    Assertions.assertTrue(content.contains(ringDto.getTitle()));
+    Assertions.assertTrue(content.contains(ringDto.getDescription()));
+  }
+
+  @Test
+  public void shouldShowRing() throws Exception {
+    final RingDto ringDto = new RingDto();
+    ringDto.setId(10L);
+    ringDto.setRadar(null);
+    ringDto.setTitle("My ring");
+    ringDto.setDescription("My ring description");
+    ringDto.setColor("#fbdb84");
+    ringDto.setPosition(0);
+    ringDto.setActive(true);
+
+    Mockito.when(ringService.findById(ringDto.getId())).thenReturn(Optional.of(ringDto));
+
+    String url = String.format("/settings/rings/show/%d", ringDto.getId());
+    MvcResult result = mockMvc.perform(get(url))
+        .andExpect(status().isOk())
+        .andReturn();
+
+    String content = result.getResponse().getContentAsString();
+    Assertions.assertTrue(content.contains(ringDto.getTitle()));
+    Assertions.assertTrue(content.contains(ringDto.getDescription()));
+  }
+
+  @Test
+  public void shouldRedirectShowRing() throws Exception {
+    MvcResult result = mockMvc.perform(get("/settings/rings/show/1"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/settings/rings"))
+        .andReturn();
+  }
+
+  @Test
+  public void shouldAddRing() throws Exception {
+    MvcResult result = mockMvc.perform(get("/settings/rings/add"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("settings/rings/add"))
+        .andExpect(model().attributeExists("ringDto"))
+        .andReturn();
+
+    String content = result.getResponse().getContentAsString();
+    Assertions.assertTrue(content.contains("title"));
+    Assertions.assertTrue(content.contains("description"));
+  }
+
+  @Test
+  public void shouldFailToCreateRingOnLowerCaseTitle() throws Exception {
+    final RingDto ringDto = new RingDto();
+    ringDto.setId(10L);
+    ringDto.setRadar(null);
+    ringDto.setTitle("My ring");
+    ringDto.setDescription("My ring description");
+    ringDto.setColor("#fbdb84");
+    ringDto.setPosition(0);
+    ringDto.setActive(true);
 
     MvcResult result = mockMvc.perform(post("/settings/rings/create")
             .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("description", ringDto.getDescription())
+            .param("title", ringDto.getTitle())
+            .sessionAttr("ringDto", ringDto))
+        .andExpect(status().isOk())
+        .andReturn();
+
+    String content = result.getResponse().getContentAsString();
+    Assertions.assertTrue(content.contains("should be uppercase"));
+  }
+
+  @Test
+  public void shouldCreateRing() throws Exception {
+    // Create a radar type
+    final RadarType radarType = new RadarType();
+    radarType.setId(1L);
+    radarType.setTitle("Technology radars 1");
+    radarType.setCode("technology_radar_1");
+    radarType.setDescription("Technology radars");
+
+    // Create a radar
+    final Radar radar = new Radar();
+    radar.setId(2L);
+    radar.setRadarType(radarType);
+    radar.setTitle("My new test Radar");
+    radar.setDescription("My awesome description");
+    radar.setPrimary(false);
+    radar.setActive(false);
+
+    final RingDto ringDto = new RingDto();
+    ringDto.setId(10L);
+    ringDto.setRadar(radar);
+    ringDto.setTitle("TRIAL");
+    ringDto.setDescription("My ring description");
+    ringDto.setColor("#fbdb84");
+    ringDto.setPosition(0);
+    ringDto.setActive(true);
+
+    MvcResult result = mockMvc.perform(post("/settings/rings/create")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("radar.id", String.valueOf(ringDto.getRadar().getId()))
             .param("title", ringDto.getTitle())
             .param("description", ringDto.getDescription())
+            .param("color", ringDto.getColor())
             .sessionAttr("ringDto", ringDto))
         .andExpect(status().is3xxRedirection())
         .andExpect(view().name("redirect:/settings/rings"))
@@ -145,117 +172,140 @@ public class RingCfgControllerTests extends AbstractControllerTests {
 
     String content = result.getResponse().getContentAsString();
   }
-   */
 
-    @Test
-    public void shouldFailToCreateRingOnBlankDescription() throws Exception {
-        final RingDto ringDto = new RingDto();
-        ringDto.setId(10L);
-        ringDto.setRadar(null);
-        ringDto.setTitle("My ring");
-        ringDto.setDescription("My ring description");
-        ringDto.setColor("#fbdb84");
-        ringDto.setPosition(0);
-        ringDto.setActive(true);
+  @Test
+  public void shouldFailToCreateRingOnBlankDescription() throws Exception {
+    final RingDto ringDto = new RingDto();
+    ringDto.setId(10L);
+    ringDto.setRadar(null);
+    ringDto.setTitle("My ring");
+    ringDto.setDescription("My ring description");
+    ringDto.setColor("#fbdb84");
+    ringDto.setPosition(0);
+    ringDto.setActive(true);
 
-        MvcResult result = mockMvc.perform(post("/settings/rings/create")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .param("title", ringDto.getTitle())
-                        .sessionAttr("ringDto", ringDto))
-                .andExpect(status().isOk())
-                .andReturn();
+    MvcResult result = mockMvc.perform(post("/settings/rings/create")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("title", ringDto.getTitle())
+            .sessionAttr("ringDto", ringDto))
+        .andExpect(status().isOk())
+        .andReturn();
 
-        String content = result.getResponse().getContentAsString();
-        Assertions.assertTrue(content.contains("must not be blank"));
-    }
+    String content = result.getResponse().getContentAsString();
+    Assertions.assertTrue(content.contains("must not be blank"));
+  }
 
-    @Test
-    public void shouldEditRing() throws Exception {
-        final RingDto ringDto = new RingDto();
-        ringDto.setId(10L);
-        ringDto.setRadar(null);
-        ringDto.setTitle("My ring");
-        ringDto.setDescription("My ring description");
-        ringDto.setColor("#fbdb84");
-        ringDto.setPosition(0);
-        ringDto.setActive(true);
+  @Test
+  public void shouldEditRing() throws Exception {
+    final RingDto ringDto = new RingDto();
+    ringDto.setId(10L);
+    ringDto.setRadar(null);
+    ringDto.setTitle("My ring");
+    ringDto.setDescription("My ring description");
+    ringDto.setColor("#fbdb84");
+    ringDto.setPosition(0);
+    ringDto.setActive(true);
 
-        Mockito.when(ringService.findById(ringDto.getId())).thenReturn(Optional.of(ringDto));
+    Mockito.when(ringService.findById(ringDto.getId())).thenReturn(Optional.of(ringDto));
 
-        String url = String.format("/settings/rings/edit/%d", ringDto.getId());
-        MvcResult result = mockMvc.perform(get(url))
-                .andExpect(status().isOk())
-                .andReturn();
-        String content = result.getResponse().getContentAsString();
+    String url = String.format("/settings/rings/edit/%d", ringDto.getId());
+    MvcResult result = mockMvc.perform(get(url))
+        .andExpect(status().isOk())
+        .andReturn();
+    String content = result.getResponse().getContentAsString();
 
-        Assertions.assertTrue(content.contains(ringDto.getTitle()));
-        Assertions.assertTrue(content.contains(ringDto.getDescription()));
-    }
+    Assertions.assertTrue(content.contains(ringDto.getTitle()));
+    Assertions.assertTrue(content.contains(ringDto.getDescription()));
+  }
 
 
-    @Test
-    public void shouldRedirectEditRing() throws Exception {
-        MvcResult result = mockMvc.perform(get("/settings/rings/edit/1"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/settings/rings"))
-                .andReturn();
-    }
+  @Test
+  public void shouldRedirectEditRing() throws Exception {
+    MvcResult result = mockMvc.perform(get("/settings/rings/edit/1"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/settings/rings"))
+        .andReturn();
+  }
 
-    /*
-    @Test
-    public void shouldUpdateRing() throws Exception {
-      final RingDto ringDto = new RingDto(10L, null, "My title", "My description", 1, true);
+  @Test
+  public void shouldUpdateRing() throws Exception {
+    // Create a radar type
+    final RadarType radarType = new RadarType();
+    radarType.setId(1L);
+    radarType.setTitle("Technology radars 1");
+    radarType.setCode("technology_radar_1");
+    radarType.setDescription("Technology radars");
 
-      MvcResult result = mockMvc.perform(post("/settings/rings/update")
-              .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-              .param("title", ringDto.getTitle())
-              .param("description", ringDto.getDescription())
-              .sessionAttr("ringDto", ringDto))
-          .andExpect(status().is3xxRedirection())
-          .andExpect(view().name("redirect:/settings/rings"))
-          .andReturn();
+    // Create a radar
+    final Radar radar = new Radar();
+    radar.setId(2L);
+    radar.setRadarType(radarType);
+    radar.setTitle("My new test Radar");
+    radar.setDescription("My awesome description");
+    radar.setPrimary(false);
+    radar.setActive(false);
 
-      String content = result.getResponse().getContentAsString();
-    }
-    */
-    @Test
-    public void shouldFailToUpdateRing() throws Exception {
-        final RingDto ringDto = new RingDto();
-        ringDto.setId(10L);
-        ringDto.setRadar(null);
-        ringDto.setTitle("My ring");
-        ringDto.setDescription("My ring description");
-        ringDto.setColor("#fbdb84");
-        ringDto.setPosition(0);
-        ringDto.setActive(true);
+    final RingDto ringDto = new RingDto();
+    ringDto.setId(10L);
+    ringDto.setRadar(radar);
+    ringDto.setTitle("TRIAL");
+    ringDto.setDescription("My ring description");
+    ringDto.setColor("#fbdb84");
+    ringDto.setPosition(0);
+    ringDto.setActive(true);
 
-        MvcResult result = mockMvc.perform(post("/settings/rings/update")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .param("title", ringDto.getTitle())
-                        .sessionAttr("ringDto", ringDto))
-                .andExpect(status().isOk())
-                .andReturn();
+    MvcResult result = mockMvc.perform(post("/settings/rings/update")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("radar.id", String.valueOf(ringDto.getRadar().getId()))
+            .param("title", ringDto.getTitle())
+            .param("description", ringDto.getDescription())
+            .param("color", ringDto.getColor())
+            .sessionAttr("ringDto", ringDto))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/settings/rings"))
+        .andReturn();
 
-        String content = result.getResponse().getContentAsString();
-        Assertions.assertTrue(content.contains("must not be blank"));
-    }
+    String content = result.getResponse().getContentAsString();
+  }
 
-    @Test
-    public void shouldDeleteRing() throws Exception {
-        final RingDto ringDto = new RingDto();
-        ringDto.setId(10L);
-        ringDto.setRadar(null);
-        ringDto.setTitle("My ring");
-        ringDto.setDescription("My ring description");
-        ringDto.setColor("#fbdb84");
-        ringDto.setPosition(0);
-        ringDto.setActive(true);
+  @Test
+  public void shouldFailToUpdateRing() throws Exception {
+    final RingDto ringDto = new RingDto();
+    ringDto.setId(10L);
+    ringDto.setRadar(null);
+    ringDto.setTitle("My ring");
+    ringDto.setDescription("My ring description");
+    ringDto.setColor("#fbdb84");
+    ringDto.setPosition(0);
+    ringDto.setActive(true);
 
-        String url = String.format("/settings/rings/delete/%d", ringDto.getId());
-        MvcResult result = mockMvc.perform(get(url))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/settings/rings"))
-                .andReturn();
-    }
+    MvcResult result = mockMvc.perform(post("/settings/rings/update")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("title", ringDto.getTitle())
+            .sessionAttr("ringDto", ringDto))
+        .andExpect(status().isOk())
+        .andReturn();
+
+    String content = result.getResponse().getContentAsString();
+    Assertions.assertTrue(content.contains("must not be blank"));
+  }
+
+  @Test
+  public void shouldDeleteRing() throws Exception {
+    final RingDto ringDto = new RingDto();
+    ringDto.setId(10L);
+    ringDto.setRadar(null);
+    ringDto.setTitle("My ring");
+    ringDto.setDescription("My ring description");
+    ringDto.setColor("#fbdb84");
+    ringDto.setPosition(0);
+    ringDto.setActive(true);
+
+    String url = String.format("/settings/rings/delete/%d", ringDto.getId());
+    MvcResult result = mockMvc.perform(get(url))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/settings/rings"))
+        .andReturn();
+  }
 
 }
