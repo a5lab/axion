04bd45819143a323ec182fbf50b0e8a52cd4be9a
A9afon <121198723+A9afon@users.noreply.github.com>
2023-04-07 12:46:37 +0300

    Explain how shouldcreatetechnologyblip works (#443)
    The shouldCreateTechnologyBlip has been fixed
    
    ---------
    
    Co-authored-by: arufanov <arufanov@gmail.com>

--- src/test/java/com/a5lab/axion/domain/technology_blip/TechnologyBlipCfgControllerTests.java
+++ src/test/java/com/a5lab/axion/domain/technology_blip/TechnologyBlipCfgControllerTests.java
@@ -3,6 +3,7 @@ package com.a5lab.axion.domain.technology_blip;
 
 import com.a5lab.axion.domain.radar.Radar;
 import com.a5lab.axion.domain.radar.RadarService;
+import com.a5lab.axion.domain.radar_type.RadarType;
 import com.a5lab.axion.domain.ring.RingDto;
 import com.a5lab.axion.domain.ring.RingService;
 import com.a5lab.axion.domain.segment.SegmentDto;
@@ -20,6 +21,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 
+
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +29,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 import com.a5lab.axion.domain.AbstractControllerTests;
 
@@ -146,8 +149,8 @@ public class TechnologyBlipCfgControllerTests extends AbstractControllerTests {
     MvcResult result = mockMvc.perform(get(url))
         .andExpect(status().isOk())
         .andReturn();
-    String content = result.getResponse().getContentAsString();
 
+    String content = result.getResponse().getContentAsString();
     Assertions.assertTrue(content.contains(technologyBlipDto.getRadar().getTitle()));
     Assertions.assertTrue(content.contains(technologyBlipDto.getTechnology().getTitle()));
     Assertions.assertTrue(content.contains(technologyBlipDto.getSegment().getTitle()));
@@ -169,8 +172,8 @@ public class TechnologyBlipCfgControllerTests extends AbstractControllerTests {
         .andExpect(view().name("settings/technology_blips/add"))
         .andExpect(model().attributeExists("technologyBlipDto"))
         .andReturn();
-    String content = result.getResponse().getContentAsString();
 
+    String content = result.getResponse().getContentAsString();
     Assertions.assertTrue(content.contains("radar"));
     Assertions.assertTrue(content.contains("Technology"));
     Assertions.assertTrue(content.contains("Segment"));
@@ -186,7 +189,7 @@ public class TechnologyBlipCfgControllerTests extends AbstractControllerTests {
 
     final SegmentDto segmentDto = new SegmentDto();
     segmentDto.setId(10L);
-    segmentDto.setRadar(null);
+    segmentDto.setRadar(radar);
     segmentDto.setTitle("My segment title");
     segmentDto.setDescription("My segment description");
     segmentDto.setPosition(1);
@@ -194,7 +197,7 @@ public class TechnologyBlipCfgControllerTests extends AbstractControllerTests {
 
     final RingDto ringDto = new RingDto();
     ringDto.setId(10L);
-    ringDto.setRadar(null);
+    ringDto.setRadar(radar);
     ringDto.setTitle("My ring title");
     ringDto.setDescription("My ring description");
     ringDto.setPosition(0);
@@ -218,25 +221,19 @@ public class TechnologyBlipCfgControllerTests extends AbstractControllerTests {
 
     MvcResult result = mockMvc.perform(post("/settings/technology_blips/create")
             .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-            .param("radar", technologyBlipDto.getRadar().getTitle())
-            .param("technology", technologyBlipDto.getTechnology().getTitle())
-            .param("segment", technologyBlipDto.getSegment().getTitle())
-            .param("ring", technologyBlipDto.getRing().getTitle())
+            .param("radar.id", String.valueOf(technologyBlipDto.getRadar().getId()))
+            .param("technology.id", String.valueOf(technologyBlipDto.getTechnology().getId()))
+            .param("segment.id", String.valueOf(technologyBlipDto.getSegment().getId()))
+            .param("ring.id", String.valueOf(technologyBlipDto.getRing().getId()))
             .sessionAttr("technologyBlipDto", technologyBlipDto))
-        .andExpect(status().isOk())
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/settings/technology_blips"))
         .andReturn();
 
     String content = result.getResponse().getContentAsString();
   }
 
   /*
-  @Test
-  public void shouldRedirectCreateTechnology_blip() throws Exception {
-    MvcResult result = mockMvc.perform(get("/settings/technology_blips/add"))
-            .andExpect(status().is3xxRedirection())
-            .andExpect(view().name("redirect:/settings/technology_blips"))
-            .andReturn();
-  }
 
   @Test
   public void shouldFailToCreateTenant() throws Exception {
@@ -266,21 +263,56 @@ public class TechnologyBlipCfgControllerTests extends AbstractControllerTests {
     String content = result.getResponse().getContentAsString();
     Assertions.assertTrue(content.contains("must not be blank"));
   }
+*/
 
   @Test
   public void shouldEditTechnologyBlip() throws Exception {
-   final Radar radar = new Radar();
-    radar.setId(10L);
-    radar.setTitle("My radar");
-    radar.setDescription("My radar description");
+    final RadarType radarType = new RadarType();
+    radarType.setId(1L);
+    radarType.setTitle("Technology radars 1");
+    radarType.setCode("technology_radar_1");
+    radarType.setDescription("Technology radars");
 
-    final SegmentDto segmentDto = new SegmentDto(10L, null, "My segment title","My segment description",1,true);
+    // Create a radar
+    final Radar radar = new Radar();
+    radar.setId(2L);
+    radar.setRadarType(radarType);
+    radar.setTitle("My new test Radar");
+    radar.setDescription("My awesome description");
+    radar.setPrimary(false);
+    radar.setActive(false);
 
-    final RingDto ringDto = new RingDto(10L, null, "My ring title", "My ring description", 1, true);
+    final SegmentDto segmentDto = new SegmentDto();
+    segmentDto.setId(10L);
+    segmentDto.setRadar(radar);
+    segmentDto.setTitle("My segment title");
+    segmentDto.setDescription("My segment description");
+    segmentDto.setPosition(1);
+    segmentDto.setActive(true);
 
-    final TechnologyDto technologyDto = new TechnologyDto(10L, "My technology", "My website", "My description", 1, true);
+    final RingDto ringDto = new RingDto();
+    ringDto.setId(10L);
+    ringDto.setRadar(radar);
+    ringDto.setTitle("My ring title");
+    ringDto.setDescription("My ring description");
+    ringDto.setPosition(0);
+    ringDto.setColor("#fbdb84");
+    ringDto.setActive(true);
 
-    final TechnologyBlipDto technologyBlipDto = new TechnologyBlipDto(10L, radar, technologyDto, segmentDto, ringDto);
+    final TechnologyDto technologyDto = new TechnologyDto();
+    technologyDto.setId(10L);
+    technologyDto.setTitle("My technology");
+    technologyDto.setWebsite("My website");
+    technologyDto.setDescription("My technology description");
+    technologyDto.setMoved(1);
+    technologyDto.setActive(true);
+
+    final TechnologyBlipDto technologyBlipDto = new TechnologyBlipDto();
+    technologyBlipDto.setId(10L);
+    technologyBlipDto.setRadar(radar);
+    technologyBlipDto.setRing(ringDto);
+    technologyBlipDto.setTechnology(technologyDto);
+    technologyBlipDto.setSegment(segmentDto);
 
     Mockito.when(technologyBlipService.findById(technologyBlipDto.getId())).thenReturn(Optional.of(technologyBlipDto));
 
@@ -288,50 +320,82 @@ public class TechnologyBlipCfgControllerTests extends AbstractControllerTests {
     MvcResult result = mockMvc.perform(get(url))
         .andExpect(status().isOk())
         .andReturn();
+
     String content = result.getResponse().getContentAsString();
 
-    Assertions.assertTrue(content.contains(technologyBlipDto.getRadar().getTitle()));
-    Assertions.assertTrue(content.contains(technologyBlipDto.getTechnology().getTitle()));
-    Assertions.assertTrue(content.contains(technologyBlipDto.getSegment().getTitle()));
-    Assertions.assertTrue(content.contains(technologyBlipDto.getRing().getTitle()));
   }
 
   @Test
   public void shouldRedirectEditTechnologyBlip() throws Exception {
-    MvcResult result = mockMvc.perform(get("/settings/technologyBlipDto/edit/1"))
+    MvcResult result = mockMvc.perform(get("/settings/technology_blips/edit/1"))
         .andExpect(status().is3xxRedirection())
-        .andExpect(view().name("redirect:/settings/technologyBlipDto"))
+        .andExpect(view().name("redirect:/settings/technology_blips"))
         .andReturn();
   }
 
   @Test
-  public void shouldUpdateTenant() throws Exception {
+  public void shouldUpdateTechnologyBlip() throws Exception {
+    final RadarType radarType = new RadarType();
+    radarType.setId(1L);
+    radarType.setTitle("Technology radars 1");
+    radarType.setCode("technology_radar_1");
+    radarType.setDescription("Technology radars");
+
+    // Create a radar
     final Radar radar = new Radar();
-    radar.setId(10L);
-    radar.setTitle("My radar");
-    radar.setDescription("My radar description");
+    radar.setId(2L);
+    radar.setRadarType(radarType);
+    radar.setTitle("My new test Radar");
+    radar.setDescription("My awesome description");
+    radar.setPrimary(false);
+    radar.setActive(false);
 
-    final SegmentDto segmentDto = new SegmentDto(10L, null, "My segment title","My segment description",1,true);
+    final SegmentDto segmentDto = new SegmentDto();
+    segmentDto.setId(10L);
+    segmentDto.setRadar(radar);
+    segmentDto.setTitle("My segment title");
+    segmentDto.setDescription("My segment description");
+    segmentDto.setPosition(1);
+    segmentDto.setActive(true);
 
-    final RingDto ringDto = new RingDto(10L, null, "My ring title", "My ring description", 1, true);
+    final RingDto ringDto = new RingDto();
+    ringDto.setId(10L);
+    ringDto.setRadar(radar);
+    ringDto.setTitle("My ring title");
+    ringDto.setDescription("My ring description");
+    ringDto.setPosition(0);
+    ringDto.setColor("#fbdb84");
+    ringDto.setActive(true);
 
-    final TechnologyDto technologyDto = new TechnologyDto(10L, "My technology title", "My website", "My technology description", 1, true);
+    final TechnologyDto technologyDto = new TechnologyDto();
+    technologyDto.setId(10L);
+    technologyDto.setTitle("My technology");
+    technologyDto.setWebsite("My website");
+    technologyDto.setDescription("My technology description");
+    technologyDto.setMoved(1);
+    technologyDto.setActive(true);
 
-    final TechnologyBlipDto technologyBlipDto = new TechnologyBlipDto(10L, radar, technologyDto, segmentDto, ringDto);
+    final TechnologyBlipDto technologyBlipDto = new TechnologyBlipDto();
+    technologyBlipDto.setId(10L);
+    technologyBlipDto.setRadar(radar);
+    technologyBlipDto.setRing(ringDto);
+    technologyBlipDto.setTechnology(technologyDto);
+    technologyBlipDto.setSegment(segmentDto);
 
     MvcResult result = mockMvc.perform(post("/settings/technology_blips/update")
-                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                    .param("radar", technologyBlipDto.getRadar().getTitle())
-                    .param("technology", technologyBlipDto.getTechnology().getTitle())
-                    .param("segment", technologyBlipDto.getSegment().getTitle())
-                    .param("ring", technologyBlipDto.getRing().getTitle())
-                    .sessionAttr("technologyBlipDto", technologyBlipDto))
-            .andExpect(status().isOk())
-            .andReturn();
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("radar.id", String.valueOf(technologyBlipDto.getRadar().getId()))
+            .param("technology.id", String.valueOf(technologyBlipDto.getTechnology().getId()))
+            .param("segment.id", String.valueOf(technologyBlipDto.getSegment().getId()))
+            .param("ring.id", String.valueOf(technologyBlipDto.getRing().getId()))
+            .sessionAttr("technologyBlipDto", technologyBlipDto))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/settings/technology_blips"))
+        .andReturn();
 
     String content = result.getResponse().getContentAsString();
   }
-  */
+
 
   @Test
   public void shouldFailToUpdateTechnologyBlip() throws Exception {
@@ -383,7 +447,6 @@ public class TechnologyBlipCfgControllerTests extends AbstractControllerTests {
         .andReturn();
 
     String content = result.getResponse().getContentAsString();
-
   }
 
   @Test
