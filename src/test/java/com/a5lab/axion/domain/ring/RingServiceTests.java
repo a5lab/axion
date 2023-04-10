8d18f5260f380a7557d32fe2558cbd92663017a4
Pavel <pavel.agf@gmail.com>
2023-04-10 21:19:32 +0300

    Add test for RingService

--- src/test/java/com/a5lab/axion/domain/ring/RingServiceTests.java
+++ src/test/java/com/a5lab/axion/domain/ring/RingServiceTests.java
@@ -4,6 +4,7 @@ import static org.mockito.ArgumentMatchers.any;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 import com.a5lab.axion.domain.AbstractServiceTests;
 
@@ -40,6 +41,74 @@ class RingServiceTests extends AbstractServiceTests {
     Assertions.assertEquals(ringDtoCollection.iterator().next().getColor(), ring.getColor());
     Assertions.assertEquals(ringDtoCollection.iterator().next().getPosition(), ring.getPosition());
     Assertions.assertEquals(ringDtoCollection.iterator().next().getDescription(), ring.getDescription());
+
+  }
+  /*
+  @Test
+  void shouldSaveRings() {
+    final RingDto ringDto = new RingDto();
+    ringDto.setId(10L);
+    ringDto.setTitle("my title");
+    ringDto.setDescription("my description");
+    ringDto.setColor("my color");
+    ringDto.setPosition(0);
+    ringDto.setActive(true);
+    //    not sure for this
+    Mockito.when(ringMapper.toDto(ringRepository.save(ringMapper.toEntity(ringDto)))).thenReturn(Optional.of(ringDto));
+    ringService.save(ringDto);
+    Mockito.verify(ringRepository).save(ringMapper.toEntity(ringDto));
+  }
+   */
+
+  @Test
+  void shouldFindByIdRings(){
+    final Ring ring = new Ring();
+    ring.setId(10L);
+    ring.setTitle("my title");
+    ring.setDescription("my description");
+    ring.setColor("my color");
+    ring.setPosition(0);
+    ring.setActive(true);
+
+    List<Ring> ringList = List.of(ring);
+    Mockito.when(ringRepository.findById(ring.getId())).thenReturn(Optional.of(ring));
+
+    ringService.findById(ring.getId());
+    Mockito.verify(ringRepository).findById(ring.getId());
+  }
+
+  @Test
+  void shouldFindByTitleRings(){
+    final Ring ring = new Ring();
+    ring.setId(10L);
+    ring.setTitle("my title");
+    ring.setDescription("my description");
+    ring.setColor("my color");
+    ring.setPosition(0);
+    ring.setActive(true);
+
+    Mockito.when(ringRepository.findByTitle(ring.getTitle())).thenReturn(Optional.of(ring));
+    ringService.findByTitle(ring.getTitle());
+
+    Mockito.verify(ringRepository).findByTitle(ring.getTitle());
+
+  }
+
+  @Test
+  void shouldDeleteRing() {
+    final Ring ring = new Ring();
+    ring.setId(10L);
+    ring.setTitle("my title");
+    ring.setDescription("my description");
+    ring.setColor("my color");
+    ring.setPosition(0);
+    ring.setActive(true);
+
+    List<Ring> ringList = List.of(ring);
+    Mockito.when(ringRepository.findById(ring.getId())).thenReturn(Optional.of(ring));
+
+    ringService.deleteById(ring.getId());
+    Mockito.verify(ringRepository).deleteById(ring.getId());
 
   }
 }
