e5b1d896b00a1561f9b53a0f88ffcc3b33dae9e9
A9afon <121198723+A9afon@users.noreply.github.com>
2023-04-13 00:52:01 +0300

    Add locale for application-test (#471)
    English locale for test has been setted. 
    ---------
    
    Co-authored-by: arufanov <arufanov@gmail.com>

--- src/test/java/com/a5lab/axion/domain/AbstractRepositoryTests.java
+++ src/test/java/com/a5lab/axion/domain/AbstractRepositoryTests.java
@@ -1,5 +1,9 @@
 package com.a5lab.axion.domain;
 
+import java.util.Locale;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -13,5 +17,6 @@ import com.a5lab.axion.config.JpaAuditingConfiguration;
     JpaAuditingConfiguration.class
 })
 @ApplicationTestBaseConfig
-public abstract class AbstractRepositoryTests {
+public abstract class AbstractRepositoryTests  extends AbstractAnyTests{
+
 }
