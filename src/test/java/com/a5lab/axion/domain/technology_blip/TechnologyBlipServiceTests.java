2a68c6980e28dad3d1d6d32dfc4dd1598a9ed2c0
Pavel <pavel.agf@gmail.com>
2023-05-07 20:48:30 +0300

    fix style blips

--- src/test/java/com/a5lab/axion/domain/technology_blip/TechnologyBlipServiceTests.java
+++ src/test/java/com/a5lab/axion/domain/technology_blip/TechnologyBlipServiceTests.java
@@ -5,24 +5,19 @@ import static org.mockito.ArgumentMatchers.any;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Filter;
 
 import com.a5lab.axion.domain.AbstractServiceTests;
 import com.a5lab.axion.domain.radar.Radar;
 import com.a5lab.axion.domain.radar.RadarRepository;
 import com.a5lab.axion.domain.ring.Ring;
-import com.a5lab.axion.domain.ring.RingMapper;
 import com.a5lab.axion.domain.ring.RingRepository;
 import com.a5lab.axion.domain.segment.Segment;
-import com.a5lab.axion.domain.segment.SegmentMapper;
 import com.a5lab.axion.domain.segment.SegmentRepository;
 import com.a5lab.axion.domain.technology.Technology;
-import com.a5lab.axion.domain.technology.TechnologyMapper;
 
 import com.a5lab.axion.domain.technology.TechnologyRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mapstruct.factory.Mappers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -31,7 +26,6 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
 
 class TechnologyBlipServiceTests extends AbstractServiceTests {
   @MockBean
@@ -258,12 +252,12 @@ class TechnologyBlipServiceTests extends AbstractServiceTests {
     technologyBlip.setTechnology(technology);
     technologyBlip.setSegment(segment);
 
-
     Mockito.when(radarRepository.findById(any())).thenReturn(Optional.of(radar));
     Mockito.when(segmentRepository.findById(any())).thenReturn(Optional.of(segment));
     Mockito.when(ringRepository.findById(any())).thenReturn(Optional.of(ring));
     Mockito.when(technologyRepository.findById(any())).thenReturn(Optional.of(technology));
     Mockito.when(technologyBlipRepository.save(any())).thenReturn(technologyBlip);
+
     TechnologyBlipDto technologyBlipDto = technologyBlipService.save(technologyBlipMapper.toDto(technologyBlip));
 
     Assertions.assertEquals(technologyBlip.getId(), technologyBlipDto.getId());
