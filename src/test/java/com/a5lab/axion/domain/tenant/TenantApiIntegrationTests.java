f10417b608d8b0cf851e7e03da241dfd4403c76c
arufanov <arufanov@gmail.com>
2023-05-10 23:19:03 +0300

    Make integration test for api (#563)
    Tenat API integration test has been implemented.

---
+++ src/test/java/com/a5lab/axion/domain/tenant/TenantApiIntegrationTests.java
@@ -0,0 +1,39 @@
+package com.a5lab.axion.domain.tenant;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.a5lab.axion.domain.AbstractIntegrationTests;
+
+
+class TenantApiIntegrationTests extends AbstractIntegrationTests {
+
+  @Autowired
+  private TenantService tenantService;
+
+  @Test
+  public void shouldGetTenants() {
+    // Create tenant
+    final TenantDto tenantDto = new TenantDto();
+    tenantDto.setTitle("My title");
+    tenantDto.setDescription("My description");
+    tenantService.save(tenantDto);
+
+    ResponseEntity<List<Tenant>> responseEntity =
+        this.restTemplate.exchange(baseUrl + port + "/api/v1/tenants", HttpMethod.GET, null,
+            new ParameterizedTypeReference<List<Tenant>>() {
+            });
+    List<Tenant> tenantList = responseEntity.getBody();
+    Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    Assertions.assertEquals(1, tenantList.size());
+
+    this.tenantService.deleteById(tenantList.iterator().next().getId());
+  }
+}
