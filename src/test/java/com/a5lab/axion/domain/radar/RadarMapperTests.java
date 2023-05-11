ddef872ab6ebf9439c82a8861a8fc1ea2bd45771
Pavel <pavel.agf@gmail.com>
2023-05-11 11:07:04 +0300

    Codding style

--- src/test/java/com/a5lab/axion/domain/radar/RadarMapperTests.java
+++ src/test/java/com/a5lab/axion/domain/radar/RadarMapperTests.java
@@ -10,15 +10,17 @@ import com.a5lab.axion.domain.AbstractMapperTests;
 import com.a5lab.axion.domain.radar_type.RadarType;
 import com.a5lab.axion.domain.radar_type.RadarTypeRepository;
 import com.a5lab.axion.domain.ring.Ring;
-import com.a5lab.axion.domain.ring.RingService;
+import com.a5lab.axion.domain.ring.RingDto;
+import com.a5lab.axion.domain.ring.RingMapper;
 import com.a5lab.axion.domain.segment.Segment;
+import com.a5lab.axion.domain.segment.SegmentDto;
+import com.a5lab.axion.domain.segment.SegmentMapper;
 import com.a5lab.axion.domain.technology_blip.TechnologyBlip;
-import com.a5lab.axion.domain.tenant.TenantService;
-import com.a5lab.axion.domain.tenant.TenantServiceImpl;
+import com.a5lab.axion.domain.technology_blip.TechnologyBlipDto;
+import com.a5lab.axion.domain.technology_blip.TechnologyBlipMapper;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mapstruct.factory.Mappers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -29,6 +31,15 @@ class RadarMapperTests  extends AbstractMapperTests {
 
   @Autowired
   private RadarMapper radarMapper;
+
+  @Autowired
+  private RingMapper ringMapper;
+
+  @Autowired
+  private SegmentMapper segmentMapper;
+
+  @Autowired
+  private TechnologyBlipMapper technologyBlipMapper;
 
   @Test
   void testToDtoWithNull() {
@@ -87,4 +98,55 @@ class RadarMapperTests  extends AbstractMapperTests {
     Assertions.assertEquals(radar.getTitle(), radarDto.getTitle());
     Assertions.assertEquals(radar.getDescription(), radarDto.getDescription());
   }
+
+  @Test
+  public void testAllListToListDto() {
+    // Create radar
+    final Radar radar = new Radar();
+    RadarDto radarDto = radarMapper.toDto(radar);
+
+    // Create ring
+    final Ring ring = new Ring();
+    ring.setId(10L);
+    ring.setTitle("My title");
+    ring.setDescription("My description");
+    ring.setColor("My color");
+    ring.setPosition(0);
+    ring.setActive(true);
+    RingDto ringDto = ringMapper.toDto(ring);
+    List<RingDto> ringDtoList = List.of(ringDto);
+
+    // Create segment
+    final Segment segment = new Segment();
+    segment.setId(10L);
+    segment.setRadar(radar);
+    segment.setTitle("My segment");
+    segment.setDescription("My segment description");
+    segment.setPosition(0);
+    segment.setActive(true);
+    SegmentDto segmentDto = segmentMapper.toDto(segment);
+    List<SegmentDto> segmentDtoList = List.of(segmentDto);
+
+    // Create technologyBlip
+    final var technologyBlip = new TechnologyBlip();
+    technologyBlip.setId(10L);
+    technologyBlip.setRadar(radar);
+    technologyBlip.setRing(ring);
+//    technologyBlip.setTechnology(technology);
+    technologyBlip.setSegment(segment);
+    TechnologyBlipDto technologyBlipDto = technologyBlipMapper.toDto(technologyBlip);
+    List<TechnologyBlipDto> technologyBlipDtoList = List.of(technologyBlipDto);
+
+    radarDto.setRingList(ringDtoList);
+    radarDto.setSegmentList(segmentDtoList);
+    radarDto.setTechnologyBlipList(technologyBlipDtoList);
+
+    Assertions.assertNotNull(radarDto.getRingList());
+    Assertions.assertNotNull(radarDto.getSegmentList());
+    Assertions.assertNotNull(radarDto.getTechnologyBlipList());
+    Assertions.assertNotNull(ringDtoList);
+    Assertions.assertNotNull(segmentDtoList);
+    Assertions.assertNotNull(technologyBlipDtoList);
+  }
+
 }
