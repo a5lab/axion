ecaf53f0ca1f1f2c6191092c08b21957212a35a6
arufanov <arufanov@gmail.com>
2023-05-16 23:09:41 +0300

    Unmapped policy for mappers has been set

--- src/main/java/com/a5lab/axion/domain/segment/SegmentMapper.java
+++ src/main/java/com/a5lab/axion/domain/segment/SegmentMapper.java
@@ -2,7 +2,6 @@ package com.a5lab.axion.domain.segment;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.a5lab.axion.config.MapperConfiguration;
@@ -11,9 +10,7 @@ import com.a5lab.axion.domain.technology_blip.TechnologyBlipMapper;
 import com.a5lab.axion.utils.PlainMapper;
 
 @Mapper(config = MapperConfiguration.class,
-    uses = {TechnologyBlipMapper.class},
-    unmappedTargetPolicy = ReportingPolicy.WARN,
-    typeConversionPolicy = ReportingPolicy.WARN)
+    uses = {TechnologyBlipMapper.class})
 public abstract class SegmentMapper implements PlainMapper<Segment, SegmentDto> {
   @Autowired
   protected RadarRepository radarRepository;
