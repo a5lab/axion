aef3bee61f4d570cbe7480ac9df378fde520dd90
Pavel <pavel.agf@gmail.com>
2023-05-22 22:48:45 +0300

    Integration test has been create

--- src/test/java/com/a5lab/axion/domain/tenant/TenantCfgIntegrationTests.java
+++ src/test/java/com/a5lab/axion/domain/tenant/TenantCfgIntegrationTests.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.result.FlashAttributeResultMatchers;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.a5lab.axion.domain.AbstractIntegrationTests;
+import com.a5lab.axion.utils.FlashMessages;
 
 class TenantCfgIntegrationTests extends AbstractIntegrationTests {
 
@@ -30,4 +33,49 @@ class TenantCfgIntegrationTests extends AbstractIntegrationTests {
 
     this.tenantService.deleteById(tenantDto.getId());
   }
+
+  @Test
+  public void shouldShowTenants() {
+    TenantDto tenantDto = new TenantDto();
+    tenantDto.setId(1L);
+    tenantDto.setTitle("My title");
+    tenantDto.setDescription("My description");
+    tenantDto = tenantService.save(tenantDto);
+    this.tenantService.findById(tenantDto.getId());
+
+    ResponseEntity<String> responseEntity =
+        restTemplate.exchange(baseUrl + port + "/settings/tenants/show/1", HttpMethod.GET, null, String.class);
+    Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    Assertions.assertTrue(responseEntity.getBody().contains(tenantDto.getTitle()));
+    Assertions.assertTrue(responseEntity.getBody().contains(tenantDto.getDescription()));
+
+    this.tenantService.deleteById(tenantDto.getId());
+  }
+
+  @Test
+  public void shouldAddTenants() {
+    ResponseEntity<String> responseEntity =
+        restTemplate.exchange(baseUrl + port + "/settings/tenants/add", HttpMethod.GET, null, String.class);
+    Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+  }
+
+  @Test
+  public void shouldCreateTenants() {
+    TenantDto tenantDto = new TenantDto();
+    tenantDto.setId(1L);
+    tenantDto.setTitle("My title");
+    tenantDto.setDescription("My description");
+    tenantDto = tenantService.save(tenantDto);
+
+    ResponseEntity<String> responseEntity =
+        restTemplate.exchange(baseUrl + port + "/settings/tenants", HttpMethod.GET, null, String.class);
+    Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    Assertions.assertTrue(responseEntity.getBody().contains(tenantDto.getTitle()));
+    Assertions.assertTrue(responseEntity.getBody().contains(tenantDto.getDescription()));
+
+
+    this.tenantService.deleteById(tenantDto.getId());
+  }
+
+
 }
