b001e082fb0d5f00ba44b7725f265aa80401b2c4
A9afon <121198723+A9afon@users.noreply.github.com>
2023-05-28 22:28:20 +0300

    RadarService has been update (#610)
    Radar filter has been implemented.
    ---------
    
    Co-authored-by: arufanov <arufanov@gmail.com>

--- src/main/java/com/a5lab/axion/domain/radar/RadarServiceImpl.java
+++ src/main/java/com/a5lab/axion/domain/radar/RadarServiceImpl.java
@@ -40,6 +40,20 @@ public class RadarServiceImpl implements RadarService {
           && !radarFilter.getTitle().isBlank()) {
         predicateList.add(builder.like(root.get("title"), radarFilter.getTitle()));
       }
+      if (radarFilter != null && radarFilter.isFilterByPrimary()) {
+        if (radarFilter.isPrimary()) {
+          predicateList.add(builder.isTrue(root.get("primary")));
+        } else {
+          predicateList.add(builder.isFalse(root.get("primary")));
+        }
+      }
+      if (radarFilter != null && radarFilter.isFilterByActive()) {
+        if (radarFilter.isActive()) {
+          predicateList.add(builder.isTrue(root.get("active")));
+        } else {
+          predicateList.add(builder.isFalse(root.get("active")));
+        }
+      }
       return builder.and(predicateList.toArray(new Predicate[] {}));
     }, pageable).map(radarMapper::toDto);
   }
