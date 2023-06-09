ac6142c79f869761fccfb4398a67fd228c21c38b
Pavel <pavel.agf@gmail.com>
2023-06-10 00:32:21 +0300

    Application.js has been removed

--- src/main/resources/static/js/filter.js
+++ src/main/resources/static/js/filter.js
@@ -1,30 +1,62 @@
-function activationCheckbox() {
-  var primary = document.getElementById('primary');
-  var filter = document.getElementById('filterByPrimary');
-  var checkingExist = document.body.contains(filter);
+document.addEventListener('DOMContentLoaded', () => {
 
-  // Existence check
-  if (checkingExist == true) {
-    // Activation condition
-    if (filter.checked == true) {
-      primary.removeAttribute('disabled');
-    } else {
-      primary.setAttribute('disabled', 'disabled');
+    function x() {
+        $("#filter-nav").click(function (event) {
+            $("#filter-form").toggle();
+        });
     }
-  }
-  var active = document.getElementById('active');
-  filter = document.getElementById('filterByActive');
-  checkingExist = document.body.contains(filter);
+    function activationCheckbox() {
 
-  // Existence check
-  if (checkingExist == true) {
-    // Activation condition
-    if (filter.checked == true) {
-      active.removeAttribute('disabled');
-    } else {
-      active.setAttribute('disabled', 'disabled');
+    var primary = document.getElementById('primary');
+    var filter = document.getElementById('filterByPrimary');
+    var checkingExist = document.body.contains(filter);
+
+    // Existence check
+    if (checkingExist == true) {
+       // Activation condition
+       if (filter.checked == true) {
+           primary.removeAttribute('disabled');
+       } else {
+          primary.setAttribute('disabled', 'disabled');
+       }
     }
-  }
-}
+    var active = document.getElementById('active');
+    filter = document.getElementById('filterByActive');
+    checkingExist = document.body.contains(filter);
 
-window.onload = activationCheckbox;
+    // Existence check
+    if (checkingExist == true) {
+        // Activation condition
+        if (filter.checked == true) {
+        active.removeAttribute('disabled');
+        } else {
+           active.setAttribute('disabled', 'disabled');
+        }
+    }
+    }
+    x();
+    activationCheckbox();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    (function () {
+            $("#filter-nav").click(function (event) {
+                $("#filter-form").toggle();
+            });
+        });
+
+    if ($('#filterByPrimary').length) {
+        if ($('#filterByPrimary')[0].checked){
+        $('#primary')[0].removeAttribute('disabled')
+        } else {
+        $('#primary')[0].setAttribute('disabled', 'disabled')
+        }
+    }
+    if ($('#filterByActive').length) {
+            if ($('#filterByActive')[0].checked){
+            $('#active')[0].removeAttribute('disabled')
+            } else {
+            $('#active')[0].setAttribute('disabled', 'disabled')
+            }
+        }
+});
