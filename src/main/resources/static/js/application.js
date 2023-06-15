0f2ecc2f2548e222ffd877cbd5a549cc0038f537
A9afon <121198723+A9afon@users.noreply.github.com>
2023-06-16 01:33:38 +0300

    Script has been implements for radar filter (#625)
    Script has been implements for radar filter

--- src/main/resources/static/js/application.js
+++
@@ -1,9 +0,0 @@
-/*
- * Helper that show/hide filter.
- * Helper assumes only one filter per page.
- */
-$(document).ready(function () {
-    $("#filter-nav").click(function (event) {
-        $("#filter-form").toggle();
-    });
-});
