abd64eefb506006e3e120b94ce1fe5f45236a272
arufanov <arufanov@gmail.com>
2023-06-16 01:25:53 +0300

    The javascript code has been removed

--- src/main/resources/static/js/filter.js
+++ src/main/resources/static/js/filter.js
@@ -9,8 +9,8 @@ $(document).ready(function () {
 });
 
 /*
- * Helper assumes the presence of a filter checkbox on the page.
  * Helper checks the status of the filter when the page is loaded.
+ * Helper assumes the presence of a filter checkbox on the page.
  */
 $(document).ready(function () {
   if ($('#filterByPrimary').length) {
@@ -23,8 +23,8 @@ $(document).ready(function () {
 });
 
 /*
- * Helper assumes the presence of a filter checkbox on the page.
  * Helper checks the status of the filter when the page is loaded.
+ * Helper assumes the presence of a filter checkbox on the page.
  */
 $(document).ready(function () {
   if ($('#filterByActive').length) {
@@ -35,54 +35,3 @@ $(document).ready(function () {
     }
   }
 });
-
-/*
-document.addEventListener('DOMContentLoaded', () => {
-  function showFilter() {
-    var filterNav = document.getElementById('filter-nav');
-    var filterForm = document.getElementById('filter-form');
-    (filterNav).click(function ( event ) {
-      (filterForm).toggle();
-    });
-  }
-  showFilter();
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  function activationCheckbox() {
-    var primary = document.getElementById('primary');
-    var filter = document.getElementById('filterByPrimary');
-    var checkingExist = document.body.contains(filter);
-
-    // Existence check
-    if (checkingExist == true) {
-       // Activation condition
-       if (filter.checked == true) {
-         primary.removeAttribute('disabled');
-       } else {
-         primary.setAttribute('disabled', 'disabled');
-       }
-    }
-  }
-  activationCheckbox();
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  function activationCheckbox() {
-    var active = document.getElementById('active');
-    var filter = document.getElementById('filterByActive');
-    var checkingExist = document.body.contains(filter);
-
-    // Existence check
-    if (checkingExist == true) {
-      // Activation condition
-      if (filter.checked == true) {
-        active.removeAttribute('disabled');
-      } else {
-        active.setAttribute('disabled', 'disabled');
-      }
-    }
-  }
-  activationCheckbox();
-});
-*/
