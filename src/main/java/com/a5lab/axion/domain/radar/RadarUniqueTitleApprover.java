bf6b011a6471657bf1d00c3cc5bf7597afe6ffee
arufanov <arufanov@gmail.com>
2023-07-03 23:26:25 +0300

    The unique title apporver has been added

---
+++ src/main/java/com/a5lab/axion/domain/radar/RadarUniqueTitleApprover.java
@@ -0,0 +1,33 @@
+package com.a5lab.axion.domain.radar;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import com.a5lab.axion.domain.ModelApprover;
+import com.a5lab.axion.domain.ModelError;
+
+@RequiredArgsConstructor
+public class RadarUniqueTitleApprover implements ModelApprover {
+
+  private final MessageSource messageSource;
+
+  private final RadarDto radarDto;
+
+  private final Radar radar;
+
+
+  @Override
+  public List<ModelError> approve() {
+    if (!Objects.equals(radarDto.getId(), radar.getId()) && radar.isPrimary()) {
+      return List.of(new ModelError("primary_invalid_primary",
+          messageSource.getMessage("radar.form.error.invalid_primary", null, LocaleContextHolder.getLocale()),
+          "primary"));
+    }
+    return new LinkedList<>();
+  }
+}
