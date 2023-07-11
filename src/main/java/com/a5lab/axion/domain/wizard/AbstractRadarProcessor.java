ab108ed75cfaa77bbe838dde35ee6d4386f5cfb9
arufanov <arufanov@gmail.com>
2023-07-12 01:30:06 +0300

    The active radar validation has been created

--- src/main/java/com/a5lab/axion/domain/wizard/AbstractRadarProcessor.java
+++ src/main/java/com/a5lab/axion/domain/wizard/AbstractRadarProcessor.java
@@ -1,66 +1,30 @@
 package com.a5lab.axion.domain.wizard;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.net.URL;
-import java.util.stream.Collectors;
-
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
 import org.springframework.context.ApplicationContext;
-import org.springframework.util.ResourceUtils;
 
-import com.a5lab.axion.domain.radar.RadarDto;
-import com.a5lab.axion.domain.radar.RadarMapper;
+import com.a5lab.axion.domain.radar.Radar;
 import com.a5lab.axion.domain.radar.RadarRepository;
+import com.a5lab.axion.domain.radar_type.RadarTypeRepository;
 
 public abstract class AbstractRadarProcessor implements RadarProcessor {
 
   protected final ApplicationContext applicationContext;
 
+  protected final RadarTypeRepository radarTypeRepository;
+
   protected final RadarRepository radarRepository;
-  protected final RadarMapper radarMapper;
 
   protected final WizardDto wizardDto;
 
-  protected RadarDto radarDto;
+  protected Radar radar = new Radar();
 
 
   public AbstractRadarProcessor(ApplicationContext applicationContext, WizardDto wizardDto) {
     this.applicationContext = applicationContext;
     this.wizardDto = wizardDto;
 
-    // Create radar repository and mapper based on application context
+    // Create repositories based on application context
+    radarTypeRepository =  applicationContext.getBean(RadarTypeRepository.class);
     radarRepository =  applicationContext.getBean(RadarRepository.class);
-    radarMapper =  applicationContext.getBean(RadarMapper.class);
-  }
-
-  public void createRadar() throws Exception {
-    // Read radars
-    URL url = ResourceUtils.getURL("classpath:database/datasets/technology_radar/radars_en.csv");
-    String fileContent = new BufferedReader(new InputStreamReader(url.openStream())).lines()
-        .collect(Collectors.joining("\n"));
-
-    String[] record = null;
-    CSVReader csvReader = new CSVReaderBuilder(new StringReader(fileContent))
-        .withCSVParser(new CSVParserBuilder().withSeparator('|').build())
-        .withSkipLines(1).build();
-    while ((record = csvReader.readNext()) != null) {
-      RadarDto radar = new RadarDto();
-      radar.setRadarTypeId(wizardDto.getRadarTypeId());
-      radar.setRadarTypeTitle(wizardDto.getRadarTypeTitle());
-      radar.setTitle(record[0]);
-      radar.setDescription(record[1]);
-      radar.setPrimary(Boolean.parseBoolean(record[2]));
-      radar.setActive(Boolean.parseBoolean(record[3]));
-      this.radarDto =  radarMapper.toDto(radarRepository.save(radarMapper.toEntity(radar)));
-    }
-  }
-
-  public void activateRadar() {
-    this.radarDto.setActive(true);
-    this.radarDto =  radarMapper.toDto(radarRepository.save(radarMapper.toEntity(radarDto)));
   }
 }
--- src/main/java/com/a5lab/axion/domain/wizard/TechnologyRadarProcessor.java
+++ src/main/java/com/a5lab/axion/domain/wizard/TechnologyRadarProcessor.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.opencsv.CSVParserBuilder;
@@ -12,20 +14,14 @@ import com.opencsv.CSVReaderBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.util.ResourceUtils;
 
+import com.a5lab.axion.domain.radar_type.RadarType;
 import com.a5lab.axion.domain.ring.Ring;
-import com.a5lab.axion.domain.ring.RingDto;
-import com.a5lab.axion.domain.ring.RingMapper;
 import com.a5lab.axion.domain.ring.RingRepository;
 import com.a5lab.axion.domain.segment.Segment;
-import com.a5lab.axion.domain.segment.SegmentDto;
-import com.a5lab.axion.domain.segment.SegmentMapper;
 import com.a5lab.axion.domain.segment.SegmentRepository;
 import com.a5lab.axion.domain.technology.Technology;
-import com.a5lab.axion.domain.technology.TechnologyDto;
-import com.a5lab.axion.domain.technology.TechnologyMapper;
 import com.a5lab.axion.domain.technology.TechnologyRepository;
-import com.a5lab.axion.domain.technology_blip.TechnologyBlipDto;
-import com.a5lab.axion.domain.technology_blip.TechnologyBlipMapper;
+import com.a5lab.axion.domain.technology_blip.TechnologyBlip;
 import com.a5lab.axion.domain.technology_blip.TechnologyBlipRepository;
 
 public class TechnologyRadarProcessor extends AbstractRadarProcessor {
@@ -38,14 +34,7 @@ public class TechnologyRadarProcessor extends AbstractRadarProcessor {
 
   private final TechnologyBlipRepository technologyBlipRepository;
 
-  private final RingMapper ringMapper;
 
-  private final SegmentMapper segmentMapper;
-
-  private final TechnologyMapper technologyMapper;
-
-  private final TechnologyBlipMapper technologyBlipMapper;
-  
 
   public TechnologyRadarProcessor(ApplicationContext applicationContext, WizardDto wizardDto) {
     super(applicationContext, wizardDto);
@@ -55,26 +44,18 @@ public class TechnologyRadarProcessor extends AbstractRadarProcessor {
     segmentRepository =  applicationContext.getBean(SegmentRepository.class);
     technologyRepository =  applicationContext.getBean(TechnologyRepository.class);
     technologyBlipRepository =  applicationContext.getBean(TechnologyBlipRepository.class);
-
-    // Create mappers based on application context
-    ringMapper =  applicationContext.getBean(RingMapper.class);
-    segmentMapper =  applicationContext.getBean(SegmentMapper.class);
-    technologyMapper =  applicationContext.getBean(TechnologyMapper.class);
-    technologyBlipMapper =  applicationContext.getBean(TechnologyBlipMapper.class);
-    
   }
 
   @Override
   public void process() throws Exception {
+    this.buildRings();
+    this.buildSegments();
     this.createRadar();
-    this.createRings();
-    this.createSegments();
     this.createTechnologies();
     this.createTechnologyBlips();
-    this.activateRadar();
   }
 
-  public void createRings() throws Exception {
+  public void buildRings() throws Exception {
     // Read rings
     URL url = ResourceUtils.getURL("classpath:database/datasets/technology_radar/rings_en.csv");
     String fileContent = new BufferedReader(new InputStreamReader(url.openStream())).lines()
@@ -85,18 +66,20 @@ public class TechnologyRadarProcessor extends AbstractRadarProcessor {
         .withCSVParser(new CSVParserBuilder().withSeparator('|').build())
         .withSkipLines(1).build();
     while ((record = csvReader.readNext()) != null) {
-      RingDto ringDto = new RingDto();
-      ringDto.setRadarId(radarDto.getId());
-      ringDto.setRadarTitle(radarDto.getTitle());
-      ringDto.setTitle(record[0]);
-      ringDto.setDescription(record[1]);
-      ringDto.setPosition(Integer.parseInt(record[2]));
-      ringDto.setColor(record[3]);
-      this.ringRepository.save(this.ringMapper.toEntity(ringDto));
+      Ring ring = new Ring();
+      ring.setRadar(radar);
+      ring.setTitle(record[0]);
+      ring.setDescription(record[1]);
+      ring.setPosition(Integer.parseInt(record[2]));
+      ring.setColor(record[3]);
+      if (radar.getRingList() == null) {
+        radar.setRingList(new LinkedList<>());
+      }
+      radar.getRingList().add(ring);
     }
   }
 
-  private void createSegments() throws Exception {
+  private void buildSegments() throws Exception {
     // Read segments
     URL url = ResourceUtils.getURL("classpath:database/datasets/technology_radar/segments_en.csv");
     String fileContent = new BufferedReader(new InputStreamReader(url.openStream())).lines()
@@ -107,13 +90,38 @@ public class TechnologyRadarProcessor extends AbstractRadarProcessor {
         .withCSVParser(new CSVParserBuilder().withSeparator('|').build())
         .withSkipLines(1).build();
     while ((record = csvReader.readNext()) != null) {
-      SegmentDto segmentDto = new SegmentDto();
-      segmentDto.setRadarId(radarDto.getId());
-      segmentDto.setRadarTitle(radarDto.getTitle());
-      segmentDto.setTitle(record[0]);
-      segmentDto.setDescription(record[1]);
-      segmentDto.setPosition(Integer.parseInt(record[2]));
-      this.segmentRepository.save(this.segmentMapper.toEntity(segmentDto));
+      Segment segment = new Segment();
+      segment.setRadar(radar);
+      segment.setTitle(record[0]);
+      segment.setDescription(record[1]);
+      segment.setPosition(Integer.parseInt(record[2]));
+      if (radar.getSegmentList() == null) {
+        radar.setSegmentList(new LinkedList<>());
+      }
+      radar.getSegmentList().add(segment);
+    }
+  }
+
+  public void createRadar() throws Exception {
+    // Find radar type
+    Optional<RadarType> radarTypeOptional = this.radarTypeRepository.findById(wizardDto.getRadarTypeId());
+
+    // Read radars
+    URL url = ResourceUtils.getURL("classpath:database/datasets/technology_radar/radars_en.csv");
+    String fileContent = new BufferedReader(new InputStreamReader(url.openStream())).lines()
+        .collect(Collectors.joining("\n"));
+
+    String[] record = null;
+    CSVReader csvReader = new CSVReaderBuilder(new StringReader(fileContent))
+        .withCSVParser(new CSVParserBuilder().withSeparator('|').build())
+        .withSkipLines(1).build();
+    while ((record = csvReader.readNext()) != null) {
+      radar.setRadarType(radarTypeOptional.get());
+      radar.setTitle(record[0]);
+      radar.setDescription(record[1]);
+      radar.setPrimary(Boolean.parseBoolean(record[2]));
+      radar.setActive(Boolean.parseBoolean(record[3]));
+      this.radarRepository.save(radar);
     }
   }
 
@@ -128,14 +136,14 @@ public class TechnologyRadarProcessor extends AbstractRadarProcessor {
         .withCSVParser(new CSVParserBuilder().withSeparator('|').build())
         .withSkipLines(1).build();
     while ((record = csvReader.readNext()) != null) {
-      TechnologyDto technologyDto = new TechnologyDto();
-      technologyDto.setTitle(record[0]);
-      technologyDto.setWebsite(record[1]);
-      technologyDto.setDescription(record[2]);
+      Technology technology = new Technology();
+      technology.setTitle(record[0]);
+      technology.setWebsite(record[1]);
+      technology.setDescription(record[2]);
 
       // Create only if not exists
-      if (this.technologyRepository.findByTitle(technologyDto.getTitle()).isEmpty()) {
-        this.technologyRepository.save(technologyMapper.toEntity(technologyDto));
+      if (this.technologyRepository.findByTitle(technology.getTitle()).isEmpty()) {
+        this.technologyRepository.save(technology);
       }
     }
   }
@@ -156,19 +164,12 @@ public class TechnologyRadarProcessor extends AbstractRadarProcessor {
       final String segmentTitle = record[2];
       final String technologyTitle = record[3];
 
-      TechnologyBlipDto technologyBlipDto = new TechnologyBlipDto();
-      technologyBlipDto.setRadarId(this.radarDto.getId());
-      technologyBlipDto.setRadarTitle(this.radarDto.getTitle());
-      Ring ring = this.ringRepository.findByTitle(ringTitle).get();
-      technologyBlipDto.setRingId(ring.getId());
-      technologyBlipDto.setRingTitle(ring.getTitle());
-      Segment segment = this.segmentRepository.findByTitle(segmentTitle).get();
-      technologyBlipDto.setSegmentId(segment.getId());
-      technologyBlipDto.setSegmentTitle(segment.getTitle());
-      Technology technology = this.technologyRepository.findByTitle(technologyTitle).get();
-      technologyBlipDto.setTechnologyId(technology.getId());
-      technologyBlipDto.setTechnologyTitle(technology.getTitle());
-      this.technologyBlipRepository.save(technologyBlipMapper.toEntity(technologyBlipDto));
+      TechnologyBlip technologyBlip = new TechnologyBlip();
+      technologyBlip.setRadar(radar);
+      technologyBlip.setRing(this.ringRepository.findByTitle(ringTitle).get());
+      technologyBlip.setSegment(this.segmentRepository.findByTitle(segmentTitle).get());
+      technologyBlip.setTechnology(this.technologyRepository.findByTitle(technologyTitle).get());
+      this.technologyBlipRepository.save(technologyBlip);
     }
   }
 }
