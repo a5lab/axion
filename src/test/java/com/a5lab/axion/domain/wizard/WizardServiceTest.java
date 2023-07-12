0de1518af7b984deb295bdb998975edfb134c328
Pavel <pavel.agf@gmail.com>
2023-07-13 00:31:04 +0300

    The Wizard test has been fixed

--- src/test/java/com/a5lab/axion/domain/wizard/WizardServiceTest.java
+++ src/test/java/com/a5lab/axion/domain/wizard/WizardServiceTest.java
@@ -12,30 +12,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.a5lab.axion.domain.AbstractServiceTests;
-import com.a5lab.axion.domain.radar.RadarDto;
-import com.a5lab.axion.domain.radar.RadarService;
+import com.a5lab.axion.domain.radar.Radar;
+import com.a5lab.axion.domain.radar.RadarRepository;
 import com.a5lab.axion.domain.radar_type.RadarType;
 import com.a5lab.axion.domain.radar_type.RadarTypeDto;
-import com.a5lab.axion.domain.ring.RingDto;
-import com.a5lab.axion.domain.ring.RingService;
-import com.a5lab.axion.domain.segment.SegmentDto;
-import com.a5lab.axion.domain.segment.SegmentService;
-import com.a5lab.axion.domain.technology.TechnologyDto;
-import com.a5lab.axion.domain.technology.TechnologyService;
-import com.a5lab.axion.domain.technology_blip.TechnologyBlipDto;
-import com.a5lab.axion.domain.technology_blip.TechnologyBlipService;
+import com.a5lab.axion.domain.radar_type.RadarTypeRepository;
+import com.a5lab.axion.domain.ring.Ring;
+import com.a5lab.axion.domain.ring.RingRepository;
+import com.a5lab.axion.domain.segment.Segment;
+import com.a5lab.axion.domain.segment.SegmentRepository;
+import com.a5lab.axion.domain.technology.Technology;
+import com.a5lab.axion.domain.technology.TechnologyRepository;
+import com.a5lab.axion.domain.technology_blip.TechnologyBlip;
+import com.a5lab.axion.domain.technology_blip.TechnologyBlipRepository;
 
 public class WizardServiceTest extends AbstractServiceTests {
+
   @MockBean
-  private RadarService radarService;
+  private RadarTypeRepository radarTypeRepository;
   @MockBean
-  private RingService ringService;
+  private RadarRepository radarRepository;
   @MockBean
-  private SegmentService segmentService;
+  private RingRepository ringRepository;
   @MockBean
-  private TechnologyService technologyService;
+  private SegmentRepository segmentRepository;
   @MockBean
-  private TechnologyBlipService technologyBlipService;
+  private TechnologyRepository technologyRepository;
+  @MockBean
+  private TechnologyBlipRepository technologyBlipRepository;
   @Autowired
   private WizardService wizardService;
 
@@ -90,71 +94,64 @@ public class WizardServiceTest extends AbstractServiceTests {
 
   @Test
   void shouldCreateTechnologyRadarEnv() throws Exception {
-    final RadarTypeDto radarTypeDto = new RadarTypeDto();
-    radarTypeDto.setId(1L);
-    radarTypeDto.setCode(RadarType.TECHNOLOGY_RADAR);
+    final RadarType radarType = new RadarType();
+    radarType.setId(1L);
+    radarType.setCode(RadarType.TECHNOLOGY_RADAR);
 
     final WizardDto wizardDto = new WizardDto();
-    wizardDto.setRadarTypeId(radarTypeDto.getId());
-    wizardDto.setRadarTypeCode(radarTypeDto.getCode());
+    wizardDto.setRadarTypeId(radarType.getId());
+    wizardDto.setRadarTypeCode(radarType.getCode());
 
-    final RadarDto radarDto = new RadarDto();
-    radarDto.setId(3L);
-    radarDto.setRadarTypeId(radarTypeDto.getId());
-    radarDto.setRadarTypeTitle(radarTypeDto.getTitle());
-    radarDto.setTitle("Technology radar");
-    radarDto.setDescription("My radar description");
+    final Radar radar = new Radar();
+    radar.setId(3L);
+    radar.setRadarType(radarType);
+    radar.setTitle("Technology radar");
+    radar.setDescription("My radar description");
 
-    final SegmentDto segmentDto = new SegmentDto();
-    segmentDto.setId(4L);
-    segmentDto.setRadarId(radarDto.getId());
-    segmentDto.setRadarTitle(radarDto.getTitle());
-    segmentDto.setTitle("Languages");
-    segmentDto.setDescription("My segment description");
-    segmentDto.setPosition(1);
+    final Segment segment = new Segment();
+    segment.setId(4L);
+    segment.setRadar(radar);
+    segment.setTitle("Languages");
+    segment.setDescription("My segment description");
+    segment.setPosition(1);
 
-    final RingDto ringDto = new RingDto();
-    ringDto.setId(5L);
-    ringDto.setRadarId(radarDto.getId());
-    ringDto.setRadarTitle(radarDto.getTitle());
-    ringDto.setTitle("ADOPT");
-    ringDto.setDescription("My ring description");
-    ringDto.setPosition(1);
-    ringDto.setColor("#fbdb84");
+    final Ring ring = new Ring();
+    ring.setId(5L);
+    ring.setRadar(radar);
+    ring.setTitle("ADOPT");
+    ring.setDescription("My ring description");
+    ring.setPosition(1);
+    ring.setColor("#fbdb84");
 
-    final TechnologyDto technologyDto = new TechnologyDto();
-    technologyDto.setId(6L);
-    technologyDto.setTitle("My technology");
-    technologyDto.setWebsite("My website");
-    technologyDto.setDescription("My technology description");
-    technologyDto.setMoved(1);
-    technologyDto.setActive(true);
+    final Technology technology = new Technology();
+    technology.setId(6L);
+    technology.setTitle("My technology");
+    technology.setWebsite("My website");
+    technology.setDescription("My technology description");
+    technology.setMoved(1);
+    technology.setActive(true);
 
-    final TechnologyBlipDto technologyBlipDto = new TechnologyBlipDto();
-    technologyBlipDto.setId(7L);
-    technologyBlipDto.setRadarId(radarDto.getId());
-    technologyBlipDto.setRingId(ringDto.getId());
-    technologyBlipDto.setTechnologyId(technologyDto.getId());
-    technologyBlipDto.setSegmentId(segmentDto.getId());
+    final TechnologyBlip technologyBlip = new TechnologyBlip();
+    technologyBlip.setId(7L);
+    technologyBlip.setRadar(radar);
+    technologyBlip.setRing(ring);
+    technologyBlip.setSegment(segment);
+    technologyBlip.setTechnology(technology);
 
-    /* TODO:
-    Mockito.when(radarService.save(any())).thenReturn(radarDto);
-    Mockito.when(ringService.save(any())).thenReturn(ringDto);
-    Mockito.when(ringService.findByTitle(any())).thenReturn(Optional.of(ringDto));
-    Mockito.when(segmentService.save(any())).thenReturn(segmentDto);
-    Mockito.when(segmentService.findByTitle(any())).thenReturn(Optional.of(segmentDto));
-    Mockito.when(technologyService.findByTitle(any())).thenReturn(Optional.of(technologyDto));
-    Mockito.when(technologyBlipService.save(any())).thenReturn(technologyBlipDto);
+    Mockito.when(radarTypeRepository.findById(any())).thenReturn(Optional.of(radarType));
+    Mockito.when(radarRepository.save(any())).thenReturn(radar);
+    Mockito.when(ringRepository.findByTitle(any())).thenReturn(Optional.of(ring));
+    Mockito.when(segmentRepository.findByTitle(any())).thenReturn(Optional.of(segment));
+    Mockito.when(technologyRepository.findByTitle(any())).thenReturn(Optional.of(technology));
+    Mockito.when(technologyBlipRepository.save(any())).thenReturn(technologyBlip);
 
     wizardService.createRadarEnv(wizardDto);
 
-    Mockito.verify(radarService, Mockito.times(2)).save(any());
-    Mockito.verify(ringService, Mockito.times(4)).save(any());
-    Mockito.verify(ringService, Mockito.times(2)).findByTitle(any());
-    Mockito.verify(segmentService, Mockito.times(4)).save(any());
-    Mockito.verify(segmentService, Mockito.times(2)).findByTitle(any());
-    Mockito.verify(technologyService, Mockito.times(4)).findByTitle(any());
-    Mockito.verify(technologyBlipService, Mockito.times(2)).save(any());
-    */
+    Mockito.verify(radarTypeRepository).findById(any());
+    Mockito.verify(radarRepository).save(any());
+    Mockito.verify(ringRepository, Mockito.times(2)).findByTitle(any());
+    Mockito.verify(segmentRepository, Mockito.times(2)).findByTitle(any());
+    Mockito.verify(technologyRepository, Mockito.times(4)).findByTitle(any());
+    Mockito.verify(technologyBlipRepository, Mockito.times(2)).save(any());
   }
 }
