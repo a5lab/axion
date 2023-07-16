7d574ac8f1b4091889898fe53e5c1ff26a4f5666
A9afon <121198723+A9afon@users.noreply.github.com>
2023-07-16 16:57:35 +0300

    The checking same errors has been implemented (#699)
    The checking same errors has been implemented

--- src/test/java/com/a5lab/axion/domain/radar/RadarCfgControllerTests.java
+++ src/test/java/com/a5lab/axion/domain/radar/RadarCfgControllerTests.java
@@ -329,6 +329,42 @@ public class RadarCfgControllerTests extends AbstractControllerTests {
   }
 
   @Test
+  public void shouldFailToCreateSecondPrimaryRadarDtoAndEmptyTitle() throws Exception {
+    final RadarDto radarDto = new RadarDto();
+    radarDto.setId(10L);
+    radarDto.setRadarTypeId(3L);
+    radarDto.setRadarTypeTitle("My radar type");
+    radarDto.setTitle("");
+    radarDto.setDescription("My description");
+    radarDto.setPrimary(true);
+    radarDto.setActive(true);
+
+    List<ModelError> modelErrorList = List.of(new ModelError(null, "must not be blank", "title"),
+        new ModelError(null, "should be only one primary radar", "primary"),
+        new ModelError(null, "size must be between 1 and 64", "title"));
+    String errorMessage = ValidationException.buildErrorMessage(modelErrorList);
+    Mockito.doThrow(new ValidationException(errorMessage, modelErrorList)).when(radarService).save(any(RadarDto.class));
+
+    MvcResult result = mockMvc.perform(post("/settings/radars/create")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("radarTypeId", String.valueOf(radarDto.getRadarTypeId()))
+            .param("description", radarDto.getDescription())
+            .sessionAttr("radarDto", radarDto))
+        .andExpect(status().isOk())
+        .andExpect(model().attributeHasFieldErrors("radarDto", "title"))
+        .andExpect(model().attributeHasFieldErrors("radarDto", "primary"))
+        .andExpect(view().name("settings/radars/add"))
+        .andReturn();
+
+    String content = result.getResponse().getContentAsString();
+    Assertions.assertTrue(content.contains("should be only one primary radar"));
+    Assertions.assertTrue(content.contains("must not be blank"));
+    Assertions.assertTrue(content.contains("size must be between 1 and 64"));
+
+    Mockito.verify(radarService).save(any(RadarDto.class));
+  }
+
+  @Test
   public void shouldFailToCreateRadarDueToNotOnePrimary() throws Exception {
     final RadarDto radarDto = new RadarDto();
     radarDto.setId(10L);
@@ -564,7 +600,6 @@ public class RadarCfgControllerTests extends AbstractControllerTests {
     Assertions.assertTrue(content.contains("active radar rings should be consecutively numbered from 0 till 3"));
     Assertions.assertTrue(content.contains("active radar segments should be consecutively numbered from 0 till 3"));
 
-
     Mockito.verify(radarService).save(any(RadarDto.class));
   }
 
@@ -596,6 +631,42 @@ public class RadarCfgControllerTests extends AbstractControllerTests {
 
     String content = result.getResponse().getContentAsString();
     Assertions.assertTrue(content.contains("is already taken"));
+
+    Mockito.verify(radarService).save(any(RadarDto.class));
+  }
+
+  @Test
+  public void shouldFailToUpdateSecondPrimaryRadarDtoAndEmptyTitle() throws Exception {
+    final RadarDto radarDto = new RadarDto();
+    radarDto.setId(10L);
+    radarDto.setRadarTypeId(3L);
+    radarDto.setRadarTypeTitle("My radar type");
+    radarDto.setTitle("");
+    radarDto.setDescription("My description");
+    radarDto.setPrimary(true);
+    radarDto.setActive(true);
+
+    List<ModelError> modelErrorList = List.of(new ModelError(null, "must not be blank", "title"),
+        new ModelError(null, "should be only one primary radar", "primary"),
+        new ModelError(null, "size must be between 1 and 64", "title"));
+    String errorMessage = ValidationException.buildErrorMessage(modelErrorList);
+    Mockito.doThrow(new ValidationException(errorMessage, modelErrorList)).when(radarService).save(any(RadarDto.class));
+
+    MvcResult result = mockMvc.perform(post("/settings/radars/update")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("radarTypeId", String.valueOf(radarDto.getRadarTypeId()))
+            .param("description", radarDto.getDescription())
+            .sessionAttr("radarDto", radarDto))
+        .andExpect(status().isOk())
+        .andExpect(model().attributeHasFieldErrors("radarDto", "title"))
+        .andExpect(model().attributeHasFieldErrors("radarDto", "primary"))
+        .andExpect(view().name("settings/radars/edit"))
+        .andReturn();
+
+    String content = result.getResponse().getContentAsString();
+    Assertions.assertTrue(content.contains("should be only one primary radar"));
+    Assertions.assertTrue(content.contains("must not be blank"));
+    Assertions.assertTrue(content.contains("size must be between 1 and 64"));
 
     Mockito.verify(radarService).save(any(RadarDto.class));
   }
