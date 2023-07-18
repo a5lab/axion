a0ada8c96154f6ec6c058976311ed0abe4cb2e08
arufanov <arufanov@gmail.com>
2023-07-19 00:49:28 +0300

    The dataIntegrity exception has been removed

--- src/main/java/com/a5lab/axion/domain/technology_blip/TechnologyBlipRepository.java
+++ src/main/java/com/a5lab/axion/domain/technology_blip/TechnologyBlipRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TechnologyBlipRepository extends JpaRepository<TechnologyBlip, Long> {
-  @Query("SELECT t FROM TechnologyBlip t WHERE t.radar_id = ?1 AND t.technology_id = ?2")
+  @Query("SELECT t FROM TechnologyBlip t WHERE t.radar = ?1 AND t.technology = ?2")
   List<TechnologyBlip> findByRadarIdAndTechnologyId(Long radarId, Long technologyId);
 }
